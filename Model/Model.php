79c005161446931a3e53e482a197302e36c71c97
Alejando <ale.aaaf@gmail.com>
2016-09-25 20:13:58 -0300

    Coneccion a bd
    retoque en la coneccion y plantillas para pedidos sql

--- Model/Model.php
+++ Model/Model.php
@@ -6,11 +6,25 @@ class Model
     private $db;
     
     public function _construct(){
-        
-        $this->base = new PDO ( "mysql:host=" . host . ";dbname=" . db, user, pass ); //las constantes estan definidas en Utils/const.php
-        
+        try {
+        	$this->base = new PDO ( "mysql:host=" . host . ";dbname=" . db, user, pass ); //las constantes estan definidas en Utils/const.php
+        } 
+		catch (PDOException $e) {
+			print "¡Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
     }
-    
+	public function querySQL($sql){
+		$conexion = $this-> _construct();
+		return $conexion->query($sql);
+	}
+		
+	public function queryPreparadaSQL($sql, $parametros){
+		$conexion = $this-> _construct();
+	    $resultado = $conexion->prepare($sql);
+	    $resultado->execute($parametros);
+	    return $resultado;
+	}
 }
 
 ?>
