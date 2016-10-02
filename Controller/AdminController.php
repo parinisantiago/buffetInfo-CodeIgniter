e757e7facf12513fa243c9477cb2848de4c2581e
parinisantiago <parinisantiago@gmail.com>
2016-10-02 01:15:09 -0300

    vista abm usuarios

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -1,12 +1,18 @@
 <?php
 require_once 'Controller/Controller.php';
 class AdminController extends Controller{
+
+	public $model;
+
 	public function __construct(){
 		parent::__contruct();
     }
 
 	public function init(){
 		$this->dispatcher->render("/Backend/adminIndexTemplate.twig");
+	}
+
+	public function abmUsuario(){
 
 	}
 }
