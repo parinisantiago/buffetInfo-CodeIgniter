0c142416ed78199d05ba21e8952aae3e77dfb6fd
parinisantiago <parinisantiago@gmail.com>
2016-10-03 19:40:27 -0300

    funciones de baja de usuario

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -1,29 +1,59 @@
 <?php
 require_once 'Controller/Controller.php';
-class AdminController extends Controller{
+class AdminController extends Controller
+{
 
 	public $model;
 
-	public function __construct(){
+
+	public function __construct()
+	{
 		parent::__contruct();
 		$this->model = new MainUserModel();
-    }
+	}
 
-	public function init(){
+
+	public function getPermission()
+	{
+		Session::init();
+		return strcmp(Session::getValue('rol'), 'admin');
+	}
+
+	public function init()
+	{
 		$this->dispatcher->render("Backend/adminIndexTemplate.twig");
 	}
 
-	public function abmUsuario(){
+	public function abmUsuario()
+	{
 		$this->dispatcher->users = $this->model->getAllUser();
 		$this->dispatcher->render('Backend/abmUsuarios.twig');
 	}
 
-	public function productos(){
+	public function productos()
+	{
 		$this->dispatcher->render("Backend/ProductosTemplate.twig");
 	}
 
-	public function productosA(){
-		
+	public function productosA()
+	{
+
 	}
+
+	public function eliminarUsuario()
+	{
+
+		if (!isset($_POST['submitButton'])) throw new Exception("Apreta el botón de envío macho");
+
+		if (!isset($_POST['idUsuario'])) throw new Exception("Faltan datos para poder eliminar el usuario");
+		else $idUsuario = $_POST['idUsuario'];
+
+		$this->dispatcher->user = $this->model->deleteUser($idUsuario);
+
+		$this->abmUsuario();
+
+	}
+
 }
+
 ?>
