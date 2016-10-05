7107c02f61f37e326ef7de3af9b96920c814621e
Alejando <ale.aaaf@gmail.com>
2016-10-04 21:01:31 -0300

    Modificar Productos

--- Model/ProductosModel.php
+++ Model/ProductosModel.php
@@ -14,7 +14,7 @@ class ProductosModel extends Model{
     }
     
     public function searchIdProducto($idProd){
-        return $this -> query(
+        return $this ->queryPreparadaSQL(
                 "SELECT p.nombre, p.marca, p.stock, p.stockMinimo, c.nombre as categoria, p.proveedor, p.precioVentaUnitario, p.descripcion, p.fechaAlta, p.idProducto
                 FROM producto p INNER JOIN categoria c ON (p.idCategoria = c.idCategoria )
                 WHERE p.eliminado = 0 and p.idProducto = :idProd" , array(idProd => $idProd));
