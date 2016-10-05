2c63d09b8f3bb86a2f2dcf72f67be6567c855162
Alejando <ale.aaaf@gmail.com>
2016-10-04 22:58:18 -0300

    updateProductos

--- Model/ProductosModel.php
+++ Model/ProductosModel.php
@@ -12,12 +12,39 @@ class ProductosModel extends Model{
                 FROM producto p INNER JOIN categoria c ON (p.idCategoria = c.idCategoria) 
                 WHERE p.eliminado = 0 ', array());
     }
-    
     public function searchIdProducto($idProd){
         return $this ->queryPreparadaSQL(
                 "SELECT p.nombre, p.marca, p.stock, p.stockMinimo, c.nombre as categoria, p.proveedor, p.precioVentaUnitario, p.descripcion, p.fechaAlta, p.idProducto
                 FROM producto p INNER JOIN categoria c ON (p.idCategoria = c.idCategoria )
                 WHERE p.eliminado = 0 and p.idProducto = :idProd" , array(idProd => $idProd));
+    }
+    public function actualizarProducto($Prod){
+        return $this -> query(
+                "UPDATE producto p 
+                SET p.nombre = :nombre,
+                    p.marca = :marca,
+                    p.stock = :stock,
+                    p.stockMinimo = :stockMinimo,
+                    c.nombre as categoria = :categoria,
+                    p.proveedor = :proveedor,
+                    p.precioVentaUnitario = :precioVentaUnitario,
+                    p.descripcion = :descripcion, 
+                    p.fechaAlta = :fechaAlta,",
+                array(nombre => $Prod["nombre"],
+                    marca => $Prod["marca"],
+                    stock => $Prod["stock"],
+                    stockMinimo => $Prod["stockMinimo"],
+                    categoria => $Prod["categoria"],
+                    proveedor => $Prod["proveedor"],
+                    precioVentaUnitario => $Prod["precioVentaUnitario"],
+                    descripcion => $Prod["descripcion"], 
+                    fechaAlta => $Prod["fechaAlt"]));
+    }
+    public function actualizarProducto($Prod){
+       /* return $this -> query(
+    INSERT INTO "nombre_tabla" ("columna1", "columna2", ...)
+VALUES ("valor1", "valor2", ...);
+        ));*/
     }
     public function deleteProducto($idProd){
         return $this -> query(
