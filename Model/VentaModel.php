2e5e5bbe75437ab6c26af2061168d3533d76fb0c
Alejando <ale.aaaf@gmail.com>
2016-10-08 11:58:18 -0300

    ventas Modelo actualizado

--- Model/VentaModel.php
+++ Model/VentaModel.php
@@ -7,31 +7,38 @@ class VentaModel extends Model{
     }
     
     public function getAllVentas($limit, $offset){
-        return $this -> queryOFFSET(
-                'SELECT p.nombre, p.marca, i.cantidad, i.precioUnitario, i.descripcion, i.fecha, it.Nombre
-                FROM producto p INNER JOIN ingresoDetalle i ON (p.idProducto=i.idProducto) INNER JOIN ingresoTipo it ON (i.idIngresoTipo=it.idIngresoTipo) 
-                WHERE p.eliminado = 0 
-                LIMIT :limit  
-                OFFSET :offset', $limit, $offset);
+        return $this -> queryOFFSET('
+            SELECT p.nombre, p.marca,i.cantidad, i.precioUnitario,i.descripcion, i.fecha, it.Nombre
+            FROM producto p INNER JOIN ingresoDetalle i ON (p.idProducto=i.idProducto) INNER JOIN ingresoTipo it ON (i.idIngresoTipo=it.idIngresoTipo) 
+            WHERE p.eliminado = 0 
+            LIMIT :limit  
+            OFFSET :offset', $limit, $offset
+        );
     }
     public function actualizarVenta($vent){
         $today=getDate();
         return $this -> query("
-                UPDATE producto 
-                SET idProducto = :idProducto,
-                    cantidad = :cantidad,
-                    precioUnitario = :precioUnitario,
-                    descripcion = :descripcion,
-                    fecha = :fecha,
-                    idTipo = :idtipo
-                WHERE idIngresoDetalle = idIngresoDetalle ",
-                array('idProducto' => $vent["idProducto"],
-                    'cantidad' => $vent["cantidad"],
-                    'precioUnitario' => $vent["precioUnitario"],
-                    'descripcion' => $vent["descripcion"],
-                    'fecha' => $vent["fecha"],
-                    'idTipo' => $vent["idTipo"],
-                ));
+            UPDATE ingresoDetalle 
+            SET idProducto = :idProducto,
+                cantidad = :cantidad,
+                precioUnitario = :precioUnitario,
+                descripcion = :descripcion,
+                fecha = :fecha,
+                idTipo = :idtipo
+            WHERE idIngresoDetalle = idIngresoDetalle ",
+            array('idProducto' => $vent["idProducto"],
+                'cantidad' => $vent["cantidad"],
+                'precioUnitario' => $vent["precioUnitario"],
+                'descripcion' => $vent["descripcion"],
+                'fecha' => $vent["fecha"],
+                'idTipo' => $vent["idTipo"],)
+        );
+    }
+    public function eliminarVenta($idVenta){
+        return $this -> query(
+                "UPDATE producto p 
+                SET p.eliminado =1 
+                WHERE p.eliminado = 0 and p.idProducto = :idProd" , array('idProd' => $idProd));
     }
 }
 ?>
