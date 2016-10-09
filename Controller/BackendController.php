ddbb70a7cf19ac6e0017e93fd067f2f6497ee1c2
parinisantiago <parinisantiago@gmail.com>
2016-10-09 19:54:59 -0300

    cambios para que liste las compras

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -79,7 +79,8 @@ class BackendController extends Controller{
  * pero lo copie de vos
  */
         $this->paginaCorrecta($this->compraModel->totalCompras());
-        $this->dispatcher->compra = $this->compraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
+        $this->dispatcher->compras = $this->compraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
+        var_dump($this->compraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']));
         $this->dispatcher->pag = $_GET['pag'];
         $this->dispatcher->method = "compraListar";
         $this->dispatcher->render("Backend/CompraListarTemplate.twig");
