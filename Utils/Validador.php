072cd69b1941c25edfe8ed03773376f61530ede3
parinisantiago <parinisantiago@gmail.com>
2016-10-10 12:03:13 -0300

    validador piola

--- Utils/Validador.php
+++ Utils/Validador.php
@@ -15,7 +15,7 @@ class Validador{
     }
 
     public function validarFecha($var, $error){
-        if (!$this->tam($var, 25) || !$this->varSet($var, $error) ||  !preg_match("/^\\d{4}\\/\\d{1,2}\\/\\d{1,2}$/", $var))
+        if (!$this->tam($var, 25) || !$this->varSet($var, $error) ||  !preg_match("(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})", $var))
             throw new Exception($error);
     }
 
