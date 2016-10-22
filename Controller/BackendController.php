787934b5ae4668ce0bb4529fb24406fd96b6a8f3
Alejando <ale.aaaf@gmail.com>
2016-10-22 00:36:05 -0300

    validador roto??
    ahora tambien tira error en modificar una venta

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -28,15 +28,12 @@ class BackendController extends Controller{
     
     /* ---Venta---*/
     public function vender(){
-        /*muestra productos a vender
-    /*****************ver el template que el boton tiene caca*/
         $this->paginaCorrecta($this->productoModel->totalProductos());
         $this->dispatcher->producto = $this->productoModel->getAllProducto($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
         $this->dispatcher->pag = $_GET['pag'];
         $this->dispatcher->render("Backend/VenderTemplate.twig");
     }
     public function venta(){
-        /***** actualizar ganancias***************************/
         $this->validator->varSet($_POST['submitButton'],"Presione el boton de envio");
         $this->validator->validarNumeros($_POST['idProducto'],"Error: no se ah encontrado la venta deseada", 3);
         $this->validator->validarNumeros($_POST['cant'],"Error: este campo solo acepta numeros", 3);
@@ -65,6 +62,12 @@ class BackendController extends Controller{
     }
 
     public function modVenta(){
+         try{
+        
+        
+        
+        
+        
         $this->validator->varSet($_POST["submitButton"], "Presione el boton de modificar");
         $this->validator->validarNumeros($_POST['idIngresoDetalle'], "Error: no se a encotrado la venta deseada",3);
         $this->validator->validarNumeros($_POST["stockViejo"], "Error: este campo solo acepta numeros", 2);
@@ -77,25 +80,26 @@ class BackendController extends Controller{
 
         $_GET['pag'] = 0;
         $this -> venderListar();
+        } catch (valException $e){
+            $this->dispatcher->producto = $_POST;
+            $this->dispatcher->mensajeError = $e->getMessage();
+            $this->venderListar();
+        }
 
     }
 
     public function ventaEliminar(){
         $this->validator->varSet($_POST['submitButton'], "Presione el botón de eliminar");
         $this->validator->varSet($_POST['idIngresoDetalle'], "Error: la venta deseada no existe.");
-
         $valor = $this->productoModel->searchIdProducto($_POST['idProducto'])->stock + $_POST['cantidad'];
         $valor .= '';
         $this->productoModel->actualizarCantProductos($_POST['idProducto'],$valor);
-
         $this->ventaModel->eliminarVenta($_POST['idIngresoDetalle']);
-  
         $_GET['pag'] = 0;
-
         $this->venderListar();
     }
-    /* ---Compra ---*/ 
 
+    /* ---Compra ---*/ 
     public function compraListar(){
         $this->paginaCorrecta($this->compraModel->totalCompras());
         $this->dispatcher->compras = $this->compraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
@@ -114,26 +118,17 @@ class BackendController extends Controller{
     public function compraAMPost(){
         $this->validarCompra($_POST);
         $producto = $this->productoModel->searchIdProducto($_POST['producto']);
-
-
         if ($_POST["idCompra"] != ""){
-
            $compra =  $this->compraModel->searchIdCompra($_POST['idCompra']);
            $nuevoStock = $producto->stock - $compra->cantidad + $_POST['cantidad'];
-
             if ($nuevoStock < 0) throw new Exception("No se puede modificar la compra porque quedaria sin stock el producto");
-
             $this->productoModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
             $this->dispatcher->compra =$this->compraModel->actualizarCompra($_POST);
-    
         }else{
-
             $this->dispatcher->compra =$this ->compraModel->insertarCompra($_POST);
-
             $nuevoStock = $producto -> stock + $_POST['cantidad'];
             $nuevoStock .= "";
             $this->productoModel->actualizarCantProductos($_POST['producto'], $nuevoStock);
-
         }
         $_GET['pag'] = 0;
         $this->dispatcher->method = "CompraListar";
@@ -141,24 +136,15 @@ class BackendController extends Controller{
     }
     
     public function compraEliminar(){
-
         $this->validator->varSet($_POST['idCompra'], "Error: no existe la compra");
         $this->validator->varSet($_POST['submitButton'], "Presione el boton de submit");
-
         $compra = $this->compraModel->searchIdCompra($_POST['idCompra']);
-
         $producto = $this->productoModel->searchIdProducto($compra->idProducto);
-
         $nuevoStock = $producto -> stock - $compra -> cantidad;
-
         if ($nuevoStock < 0) throw new Exception("Error: no se puede eliminar la compra, ya que se han vendido productos");
         $nuevoStock .= "";
-
         $this->productoModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
-
         $this->dispatcher->compra =$this ->compraModel->eliminarCompra($compra->idCompra);
-
-
         $_GET['pag'] = 0;
         $this->dispatcher->method = "CompraListar";
         $this->compraListar();
@@ -171,8 +157,8 @@ class BackendController extends Controller{
         $this->validator->validarNumerosPunto($var['precioUnitario'],"Error: error en precio unitario",50);
 
     }
+
     /* ---Productos---*/
-    
     public function productosListar(){
         $this->paginaCorrecta($this->productoModel->totalProductos());
         $this->dispatcher->producto = $this->productoModel->getAllProducto($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
@@ -188,7 +174,6 @@ class BackendController extends Controller{
         $this->dispatcher->render("Backend/ProductosAMTemplate.twig");
     }
     public function productosAMPost(){
-        /*ver botones de venta, solo son del admin*/
         try{
         $this->validarProductos($_POST);
         if ($_POST["idProducto"] != ""){
@@ -205,7 +190,6 @@ class BackendController extends Controller{
             $this->productosAM();
         }
     }
-
     public function productosEliminar(){
      $this->dispatcher->producto =$this ->productoModel->deleteProducto($_POST["idProducto"]);
         $_GET['pag'] = 0;
@@ -220,22 +204,16 @@ class BackendController extends Controller{
           $this->validator->validarNumeros($var['stock'],"Error: el campo 'Stock' solo admite numeros.",3);
           $this->validator->validarNumeros($var['stockMinimo'],"Error: el 'Stock Minimo' nombre solo admite letras.",3);
           $this->validator->validarNumeros($var['idCategoria'],"No existe la categoria.",3);
-           
           $this->validator->validarNumerosPunto($var['precioVentaUnitario'],"Error: solo se admite el formato xxx.xx",5);
           if (! $this->categoriaModel->getCategoriaById($var['categoria'])) throw new Exception("Error:No existe la categoria");
-}
-
+    }
+    
     /*--- paginacion ---*/
-
     public function paginaCorrecta($total){
-
         if (! isset($_GET['pag'])) throw new Exception('Error:No hay una página que mostrar');
-
         elseif ($total->total <= $_GET['pag'] *  $this->conf->getConfiguracion()->cantPagina){  $_GET['pag'] = 0; $_GET['offset'] = 0;}
         else $_GET['offset'] = $this->conf->getConfiguracion()->cantPagina * $_GET['pag'];
         if ($_GET['offset'] < 0) $_GET['offset'] = 0;
         $_GET['offset'] .= "";
-
     }
-    
 }
