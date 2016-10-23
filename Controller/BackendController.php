fb3158def24b4d5d46e8c85320ae5af20b56f8b8
Alejando <ale.aaaf@gmail.com>
2016-10-23 20:19:35 -0300

    subida de fotos
    VIOLA EL MVC ojo

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -141,6 +141,38 @@ class BackendController extends Controller{
         $this->dispatcher->method = "CompraListar";
         $this->compraListar();
     }
+    public function subirFoto(){
+        $target_path = "uploads/";
+        $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+            echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido";
+        }else{
+            echo "Ha ocurrido un error, trate de nuevo!";
+        }
+    } 
+    public function uploader(){
+        $uploadedfileload="true";
+        $uploadedfile_size=$_FILES['uploadedfile'][size];
+        echo $_FILES[uploadedfile][name];
+        if ($_FILES[uploadedfile][size]>200000)
+           {$msg=$msg."El archivo es mayor que 200KB, debes reduzcirlo antes de subirlo<BR>";
+        $uploadedfileload="false";}
+
+        if (!($_FILES[uploadedfile][type] =="image/pjpeg" OR $_FILES[uploadedfile][type] =="image/gif"))
+        {$msg=$msg." Tu archivo tiene que ser JPG o GIF. Otros archivos no son permitidos<BR>";
+        $uploadedfileload="false";}
+
+        $file_name=$_FILES[uploadedfile][name];
+        $add="uploads/$file_name";
+        if($uploadedfileload=="true"){
+
+        if(move_uploaded_file ($_FILES[uploadedfile][tmp_name], $add)){
+        echo " Ha sido subido satisfactoriamente";
+        }else{echo "Error al subir el archivo";}
+
+        }else{echo $msg;}
+    }
+    
     public function validarCompra($var){
         $this->validator->varSet($var['submit'],"Presione el boton de submit");
         if (! $this->compraModel->searchIdProveedor($var['proveedor'])) throw new Exception("Error: No existe el proveedor");
