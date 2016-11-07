fd44970dae4610c56825604a815bbb1a24ee1b7e
Alejando <ale.aaaf@gmail.com>
2016-11-06 23:42:40 -0300

    no me toma el for me voy a dormir

--- Model/MenuModel.php
+++ Model/MenuModel.php
@@ -22,12 +22,11 @@ class MenuModel extends Model{
     public function getMenuToday(){
         $today=getDate();
         return $this->queryPreparadaSQL('
-            SELECT *
+            SELECT m.foto, p.nombre, p.precioVentaUnitario, p.descripcion
             FROM menu m
             INNER JOIN menuProducto mp ON (mp.idMenu = m.idMenu)
             INNER JOIN producto p ON (mp.idProducto = p.idProducto)
-            WHERE m.fecha = :fecha
-            AND m.eliminado = 0',
+            WHERE m.fecha = :fecha and m.eliminado = 0 and p.eliminado =0 ',
             array('fecha' => $today['year']."-".$today['mon']."-".$today['mday']));
     }
     
