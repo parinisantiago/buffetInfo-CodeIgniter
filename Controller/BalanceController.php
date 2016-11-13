65c040c9ff7d2f513363b7d28e75a4143fcd32d3
parinisantiago <parinisantiago@gmail.com>
2016-11-13 02:38:38 -0300

    refactoring

--- Controller/BalanceController.php
+++ Controller/BalanceController.php
@@ -24,41 +24,61 @@ class BalanceController extends Controller
         try
         {
             $this->validator->validarFecha($_POST['fecha'], "La fecha ingresada no es válida");
+            $fecha=$_POST['fecha'];
         }
         catch (valException $e){
             echo "apa la bocha";
         }
 
 
-        $ingreso = $this->balance->ingresoDia($_POST['fecha']);
-
-        (empty($ingreso->total))? $ingreso = "0" : $ingreso = $ingreso->total;
-
-
-        $egreso = $this->balance->egresoDia($_POST['fecha']);
-
-        (empty($egreso->total))? $egreso = "0" : $egreso = $egreso->total;
-
-        $totalProductos = $this->balance->productosEgresoDia($_POST['fecha']);
-        
-
-        $chart = new PieChart(500,250);
-        $dataSet = new XYDataSet();
-
-
-        foreach ($totalProductos as $producto)
-        {
-            $dataSet->addPoint(new Point($producto->nombre,$producto->cant));
-        }
-
-        $chart->setTitle('Cantidad de productos vendidos para el dia '.$_POST['fecha']);
-        $chart->setDataSet($dataSet);
-        $chart->render('libchart/demo/generated/demo.png');
-        chmod('libchart/demo/generated/demo.png', 0777);
-        $image = imagecreatefrompng('libchart/demo/generated/demo.png');
+        $this->graficoBarraDia($fecha);
+        $this->graficoTortaDia($fecha);
 
         $this->dispatcher->render('Backend/balanceTemplate.twig');
 
+    }
+
+    protected function graficoTortaDia($fecha)
+    {
+        $totalProductos = $this->balance->productosEgresoDia($fecha);
+
+
+        $chart = new PieChart(500, 250);
+        $dataSet = new XYDataSet();
+
+
+        foreach ($totalProductos as $producto) {
+            $dataSet->addPoint(new Point($producto->nombre, $producto->cant));
+        }
+
+        $chart->setTitle('Cantidad de productos vendidos para el dia ' . $fecha);
+        $chart->setDataSet($dataSet);
+        $chart->render('libchart/demo/generated/demo.png');
+        $image = imagecreatefrompng('libchart/demo/generated/demo.png');
+    }
+
+    protected function graficoBarraDia($fecha)
+    {
+        $ingreso = $this->balance->ingresoDia($fecha);
+
+        (empty($ingreso->total)) ? $ingreso = "0" : $ingreso = $ingreso->total;
+
+
+        $egreso = $this->balance->egresoDia($fecha);
+
+        (empty($egreso->total)) ? $egreso = "0" : $egreso = $egreso->total;
+
+        $balance = $ingreso - $egreso;
+
+
+        $chart = new VerticalBarChart(500,250);
+        $dataSet = new XYDataSet();
+        $dataSet->addPoint(new Point($fecha, $balance));
+        $chart->setTitle('Balance para el dia ' . $fecha);
+        $chart->setTitle('Balance para el dia ' . $fecha);
+        $chart->setDataSet($dataSet);
+        $chart->render('libchart/demo/generated/demo2.png');
+        $image = imagecreatefrompng('libchart/demo/generated/demo2.png');
     }
 
 }
