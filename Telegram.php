775d3cb0523b0c0db2546507d077824d1ae617f9
Alejando <ale.aaaf@gmail.com>
2016-11-25 12:43:15 -0300

    test

--- Telegram.php
+++ Telegram.php
@@ -47,7 +47,7 @@ require_once 'Utils/Const.php';
                 $msg['text'] = 'Hola ';
                 $menu = $menuModel->getMenuToday();
                 if ($menu) {
-                    $msg['text'] = 'El menú del día es: ' . $menu['nombre'] . ' ' . $menu['descripcion'];
+                    $msg['text'] = 'El menú del día es: ' . $menu["nombre"] . ' ' . $menu["descripcion"];
                 } else {
                     $msg['text'] = 'No han planificado ningun menú para hoy';
                 }
@@ -59,7 +59,7 @@ require_once 'Utils/Const.php';
                 $fecha = $today['year'] . "-" . $today['mon'] . "-" . ($today['mday'] + 1);
                 $menu = $menuModel->getMenuByDia($fecha,1,0);
                 if ($menu) {
-                    $msg['text'] = 'El menú de mañana es: ' . $menu["nombre"] . ' ' . $menu["descripcion"];
+                    $msg['text'] = 'El menú de mañana es: ' . $menu . ' ' . $menu["descripcion"];
                 } else {
                     $msg['text'] = 'No han planificado ningun menú para mañana :(';
                 }
