2ca30267d733072f08195720eca19512d2c9e3f6
Alejando <ale.aaaf@gmail.com>
2016-11-28 10:58:58 -0300

    test

--- Telegram.php
+++ Telegram.php
@@ -47,39 +47,14 @@ require_once 'Utils/Const.php';
                 $msg['text'] = 'Hola ';
                 $menu = $menuModel->getMenuToday();
                 if ($menu) {
-                    foreach ($menu as $clave=>$valor){
-                        $msg['text'] .= $menu[$clave];
-                         $msg['text'] .= $menu[$valor];
+                    foreach ($menu as $producto){
+                        $msg['text'] .= $producto->nombre;
+                         $msg['text'] .= $producto->descripcion;
                     }
                 } else {
                     $msg['text'] = 'No han planificado ningun menú para hoy';
                 }
                 $msg['reply_to_message_id'] = null;
-                break;
-            case '/hoy1':
-                
-                $menu = $menuModel->getMenuToday();
-                foreach ($menu as $clave=>$valor){
-                        $msg['text'] .= $clave;
-                }
-                break;
-            case '/hoy2':
-                $msg['text'] = 'Hola ';
-                $menu = $menuModel->getMenuToday();
-                foreach ($menu as $clave=>$valor){
-                         $msg['text'] .= $valor;
-                }
-            break;
-            case '/hoy3':
-                $msg['text'] = 'Hola ';
-                $menu = $menuModel->getMenuToday();
-                 $msg['text'] .= $menu['nombre'];
-                break;
-            case '/hoy4':
-                $msg['text'] = 'Hola ';
-                
-                $menu = $menuModel->getMenuToday();
-                $msg['text'] .= $menu;
                 break;
             case '/maniana':
                 $msg['text'] = 'Hola ';
