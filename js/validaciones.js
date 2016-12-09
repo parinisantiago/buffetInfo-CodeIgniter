751cf387655c03ebe9cbe4cf38270818e3e0bd17
parinisantiago <parinisantiago@gmail.com>
2016-12-08 23:00:05 -0300

    cambios en las validaciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,9 +1,12 @@
 function valLetras(text, form){
 
+    var error=  document.createElement("P");
+
+
     if( text.value.length == 0){
 
 
-        var error=  document.createElement("P");
+        error=  document.createElement("P");
 
         error.setAttribute("class", "error");
         error.innerHTML ="Debe completar este campo";
@@ -11,7 +14,7 @@ function valLetras(text, form){
 
     }else if ( /[A-Za-z]+/.exec(text.value) != text.value){
 
-        var error=  document.createElement("P");
+        error=  document.createElement("P");
 
         error.setAttribute("class", "error");
             error.innerHTML ="Solo se aceptan letras";
@@ -24,10 +27,13 @@ function valLetras(text, form){
 
 function valLetrasYNumeros(text, form){
 
+    var error=  document.createElement("P");
+
+
     if( text.value.length == 0){
 
 
-        var error=  document.createElement("P");
+        error=  document.createElement("P");
 
         error.setAttribute("class", "error");
         error.innerHTML ="Debe completar este campo";
@@ -35,7 +41,7 @@ function valLetrasYNumeros(text, form){
 
     }else if ( /[A-Za-z09]+/.exec(text.value) != text.value){
 
-        var error=  document.createElement("P");
+        error=  document.createElement("P");
 
         error.setAttribute("class", "error");
         error.innerHTML ="Solo se aceptan letras o numeros";
@@ -46,85 +52,108 @@ function valLetrasYNumeros(text, form){
     return(false);
 }
 
-function valEmail(text){
+function valEmail(text, form){
+    var error=  document.createElement("P");
+
     if( text.value.length == 0){
-        text.parentNode.setAttribute("class", "form-group has-warning has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-warning-sign form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "debe completar este campo";
+        error=  document.createElement("P");
+
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe completar este campo";
+        form.insertBefore(error, text);
 
     }else if ( /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/.exec(text.value) != text.value){
-        text.parentNode.setAttribute("class", "form-group has-error has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-remove form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "No puede poseer numeros ni simbolos";
+        error=  document.createElement("P");
 
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe poseer el formato usuario@correo";
+        form.insertBefore(error, text);
     } else {
         return(true);
     }
     return(false);
 }
 
-function valNumeros(text){
-    if( text.value.length == 0){
-        text.parentNode.setAttribute("class", "form-group has-warning has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-warning-sign form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "debe completar este campo";
+function valNumeros(text, form){
+    var error=  document.createElement("P");
 
+    if( text.value.length == 0){
+
+        error=  document.createElement("P");
+
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe completar este campo";
+        form.insertBefore(error, text);
     }else if ( /[0-9]+/.exec(text.value) != text.value){
-        text.parentNode.setAttribute("class", "form-group has-error has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-remove form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "No puede poseer letras ni simbolos";
 
+        error=  document.createElement("P");
+
+        error.setAttribute("class", "error");
+        error.innerHTML ="Solo se aceptan números";
+        form.insertBefore(error, text);
     } else {
         return(true);
     }
     return(false);
 }
 
-function valReales(text){
-    if( text.value.length == 0){
-        text.parentNode.setAttribute("class", "form-group has-warning has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-warning-sign form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "debe completar este campo";
+function valReales(text, form){
+    var error=  document.createElement("P");
 
+    if( text.value.length == 0){
+        error=  document.createElement("P");
+
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe completar este campo";
+        form.insertBefore(error, text);
     }else if ( /[0-9,]+/.exec(text.value) != text.value){
-        text.parentNode.setAttribute("class", "form-group has-error has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-remove form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "Debe segui el siguiente formato: 00,00";
+        error=  document.createElement("P");
 
+        error.setAttribute("class", "error");
+        error.innerHTML ="Solo se acepta el formato 00,00";
+        form.insertBefore(error, text);
     } else {
         return(true);
     }
     return(false);
 }
 
-function valPalabras(text){
-    if( text.value.length == 0){
-        text.parentNode.setAttribute("class", "form-group has-warning has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-warning-sign form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "debe completar este campo";
+function valPalabras(text, form){
+    var error=  document.createElement("P");
 
+    if( text.value.length == 0){
+        error=  document.createElement("P");
+
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe completar este campo";
+        form.insertBefore(error, text);
     }else if ( /[A-Za-z áéíóúàèìòùñ]+/.exec(text.value) != text.value){
-        text.parentNode.setAttribute("class", "form-group has-error has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-remove form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "No puede poseer simbolos";
+        error=  document.createElement("P");
 
+        error.setAttribute("class", "error");
+        error.innerHTML ="Soo se aceptan letras, numeros y espacios";
+        form.insertBefore(error, text);
     } else {
         return(true);
     }
     return(false);
 }
 
-function valFechas(text){
+function valFechas(text, form){
+    var error=  document.createElement("P");
+
     if( text.value.length == 0){
-        text.parentNode.setAttribute("class", "form-group has-warning has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-warning-sign form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "debe completar este campo";
+        error=  document.createElement("P");
 
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe completar este campo";
+        form.insertBefore(error, text);
     }else if ( /[0-9]+/.exec(text.value) != text.value){
-        text.parentNode.setAttribute("class", "form-group has-error has-feedback");
-        document.getElementById("glyphicon-nomTipo").setAttribute("class","glyphicon glyphicon-remove form-control-feedback");
-        document.getElementById("helpBlock-nomTipo").innerHTML = "Debe segui el siguiente formato: YYYY-MM-DD";
+        error=  document.createElement("P");
 
+        error.setAttribute("class", "error");
+        error.innerHTML ="Debe poseer el formato YYYY-MM-DD";
+        form.insertBefore(error, text);
     } else {
         return(true);
     }
@@ -132,12 +161,46 @@ function valFechas(text){
 }
 
 function valUsuario() {
+
+    document.getElementById("error").setAttribute("hidden","true");
+
+
     var bol = true;
     var form = document.getElementById("form");
     var text= document.getElementById("usuario");
     var pass = document.getElementById("clave");
-    if(valLetrasYNumeros(text, form) || valLetrasYNumeros(pass, form)){
+    var nombre = document.getElementById("nombre");
+    var apellido = document.getElementById("apellido");
+    var documento = document.getElementById("documento");
+    var email = document.getElementById("email");
+    var telefono = document.getElementById("telefono");
 
+
+    if(valLetrasYNumeros(text, form)){
+        bol = false;
+    }
+    if(valLetrasYNumeros(pass, form))
+    {
+        bol = false;
+    }
+    if(valPalabras(nombre, form))
+    {
+        bol = false;
+    }
+    if(valPalabras(apellido, form))
+    {
+        bol = false;
+    }
+    if(valNumeros(documento, form))
+    {
+        bol = false;
+    }
+    if(valEmail(email, form))
+    {
+        bol = false;
+    }
+    if(valNumeros(telefono, form))
+    {
         bol = false;
     }
     return(bol);
