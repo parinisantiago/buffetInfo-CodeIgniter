67c7df25a1242f524595f2cd1f7cd9715b5c48f1
parinisantiago <parinisantiago@gmail.com>
2017-03-29 19:54:29 -0300

    cambio las queries del MenuModel por las nuevas

--- application/models/MenuModel.php
+++ application/models/MenuModel.php
@@ -8,27 +8,54 @@ class MenuModel extends Model{
     }
 
     public function totalProd($id){
-        return $this->queryPreparadaSQL('
-            SELECT COUNT(*) AS total  
-            FROM menuProducto mp
-            INNER JOIN menu m
-            WHERE m.fecha = : id
-            AND m.eliminado = 0
-            ', array("id" => $id));
+
+        $this->db->select('COUNT(*) as total');
+        $this->db->from('menuProducto mp');
+        $this->db->join('menu m');
+        $this->db->where('m.fecha',$id);
+        $this->db->where('m.eliminado', 0);
+        $this->db->get()->result();
+        /* return $this->queryPreparadaSQL('
+             SELECT COUNT(*) AS total
+             FROM menuProducto mp
+             INNER JOIN menu m
+             WHERE m.fecha = : id
+             AND m.eliminado = 0
+             ', array("id" => $id));*/
     }
 
     public function getMenuToday2(){
         $today=getDate();
-        return $this->queryTodasLasFilas('
+        $this->db->select('m.idMenu, m.foto, p.nombre, p.precioVentaUnitario, p.descripcion');
+        $this->db->from('menu m');
+        $this->db->join('menuProducto mp', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('m.fecha',$today['year']."-".$today['mon']."-".$today['mday']);
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->where('m.habilitado', 0);
+        $this->db->get()->result();
+       /* return $this->queryTodasLasFilas('
             SELECT m.idMenu, m.foto, p.nombre, p.precioVentaUnitario, p.descripcion
             FROM menu m
             INNER JOIN menuProducto mp ON (mp.idMenu = m.idMenu)
             INNER JOIN producto p ON (mp.idProducto = p.idProducto)
             WHERE m.fecha = :fecha and m.eliminado = 0 and p.eliminado =0 and m.habilitado = 0',
-            array('fecha' => $today['year']."-".$today['mon']."-".$today['mday']));
+            array('fecha' => $today['year']."-".$today['mon']."-".$today['mday']));*/
     }
 
     public function getMenuByDia2($limit, $offset, $fecha){
+        $this->db->select('*');
+        $this->db->from('menu m');
+        $this->db->join('menuProducto mp', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('m.fecha', $fecha);
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('m.habilitado', 0);
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+/*
         return $this->queryOFFSETDIA('
             SELECT *  
             FROM menu m
@@ -39,10 +66,21 @@ class MenuModel extends Model{
             AND m.habilitado = 0
             LIMIT :limit
             OFFSET :offset
-        ', $limit, $offset, $fecha);
+        ', $limit, $offset, $fecha);*/
     }
 
     public function getMenuByDia($limit, $offset, $fecha){
+
+        $this->db->select('*');
+        $this->db->from('menu m');
+        $this->db->join('menuProducto mp', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('m.fecha', $fecha);
+        $this->db->where('m.eliminado', 0);
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+/*
         return $this->queryOFFSETDIA('
             SELECT *  
             FROM menu m
@@ -52,88 +90,163 @@ class MenuModel extends Model{
             AND m.eliminado = 0
             LIMIT :limit
             OFFSET :offset
-        ', $limit, $offset, $fecha);
+        ', $limit, $offset, $fecha);*/
     }
     public function getMenuToday(){
         $today=getDate();
-        return $this->queryTodasLasFilas('
-            SELECT m.idMenu, m.foto, p.nombre, p.precioVentaUnitario, p.descripcion
-            FROM menu m
-            INNER JOIN menuProducto mp ON (mp.idMenu = m.idMenu)
-            INNER JOIN producto p ON (mp.idProducto = p.idProducto)
-            WHERE m.fecha = :fecha and m.eliminado = 0 and p.eliminado =0 ',
-            array('fecha' => $today['year']."-".$today['mon']."-".$today['mday']));
+        $this->db->select('m.idMenu, m.foto, p.nombre, p.precioVentaUnitario, p.descripcion');
+        $this->db->from('menu m');
+        $this->db->join('menuProducto mp', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('m.fecha', $today['year']."-".$today['mon']."-".$today['mday']);
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->get()->result();
+
+        /*
+                return $this->queryTodasLasFilas('
+                    SELECT m.idMenu, m.foto, p.nombre, p.precioVentaUnitario, p.descripcion
+                    FROM menu m
+                    INNER JOIN menuProducto mp ON (mp.idMenu = m.idMenu)
+                    INNER JOIN producto p ON (mp.idProducto = p.idProducto)
+                    WHERE m.fecha = :fecha and m.eliminado = 0 and p.eliminado =0 ',
+                    array('fecha' => $today['year']."-".$today['mon']."-".$today['mday'])); */
     }
     
     public function getProdNotInMenu($idMenu)
     {
-        return $this -> queryTodasLasFilas('
+        $this->db->select('*');
+        $this->db->from('producto');
+        $this->db->where('eliminado', 0);
+        $this->db->where('eliminado', 0);
+        $this->db->where('stock >', 0);
+        $this->db->where_not('idProducto', $this->db->select('idProducto')->from('menuProducto')->where('idMenu',$idMenu));
+        $this->db->get()->result();
+
+  /*      return $this -> queryTodasLasFilas('
         SELECT * 
         FROM producto 
         WHERE eliminado = 0
         AND stock > 0
         AND idProducto NOT IN (SELECT idProducto FROM menuProducto WHERE idMenu = :idMenu )',
-        array('idMenu'=> $idMenu));
+        array('idMenu'=> $idMenu));*/
     }
 
     public function getMenuDia($fecha)
     {
-        return $this->queryPreparadaSQL('
-            SELECT *
-            FROM menu
-            WHERE fecha = :fecha
-            AND eliminado = 0',
-            array("fecha" => $fecha));
+        $this->db->select('*');
+        $this->db->from('menu');
+        $this->db->where('fecha', $fecha);
+        $this->db->where('eliminado', 0);
+        $this->db->get()->result();
+
+        /*   return $this->queryPreparadaSQL('
+               SELECT *
+               FROM menu
+               WHERE fecha = :fecha
+               AND eliminado = 0',
+               array("fecha" => $fecha));*/
     }
 
     public function totalMenu(){
-        return $this->queryPreparadaSQL('
+        $this->db->select('COUNT(*) AS total');
+        $this->db->from('menu m');
+        $this->db->join('producto p', 'm.idProducto=p.idProducto');
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->order_by('m.idMenu', 'ASC');
+        $this->db->get()->result();
+
+        /*return $this->queryPreparadaSQL('
             SELECT COUNT(*) AS total  
             FROM menu m INNER JOIN producto p ON (m.idProducto=p.idProducto)
             WHERE m.eliminado = 0 and p.eliminado=0
-            ORDER BY m.idMenu', array());
+            ORDER BY m.idMenu', array());*/
     }
 
     public function idMenu($id)
     {
-        return $this->queryPreparadaSQL('
-            SELECT *
-            FROM menu
-            WHERE idMenu = :id
-            AND eliminado = 0',
-            array("id" => $id));
+        $this->db->select('*');
+        $this->db->from('menu');
+        $this->db->where('idMenu', $id);
+        $this->db->where('eliminado', 0);
+        $this->db->get()->result();
+
+        /*  return $this->queryPreparadaSQL('
+              SELECT *
+              FROM menu
+              WHERE idMenu = :id
+              AND eliminado = 0',
+              array("id" => $id));*/
     }
 
     public function searchIdMenu($idMenu){
-        return $this ->queryPreparadaSQL("
-            SELECT m.idMenu,m.idProducto,m.fecha,m.foto,m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
-            FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto) 
-            WHERE  m.eliminado=0 and p.eliminado=0 and m.idMenu = :idMenu" , array('idMenu' => $idMenu));
+        $this->db->select('m.idMenu,m.idProducto,m.fecha,m.foto,m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado');
+        $this->db->from('menu m');
+        $this->db->join('producto p', 'm.idProducto=p.idProducto');
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->where('m.idMenu', $idMenu);
+        $this->db->get()->result();
+
+
+        /*    return $this ->queryPreparadaSQL("
+                SELECT m.idMenu,m.idProducto,m.fecha,m.foto,m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
+                FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto)
+                WHERE  m.eliminado=0 and p.eliminado=0 and m.idMenu = :idMenu" , array('idMenu' => $idMenu));*/
     }
+
     public function getAllMenu($limit, $offset){
-        return $this -> queryOFFSET('
-            SELECT m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
-            FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto) 
-            WHERE m.eliminado = 0 and p.eliminado=0
-            ORDER BY p.nombre
-            LIMIT :limit  
-            OFFSET :offset', $limit, $offset
-            );
+        $this->db->select('m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado');
+        $this->db->from('menu m');
+        $this->db->join('producto p', 'm.idProducto=p.idProducto');
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->order_by('p.nombre', 'ASC');
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+
+        /* return $this -> queryOFFSET('
+             SELECT m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
+             FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto)
+             WHERE m.eliminado = 0 and p.eliminado=0
+             ORDER BY p.nombre
+             LIMIT :limit
+             OFFSET :offset', $limit, $offset
+             ); */
     }
 
     public function getAllMenuDia($limit, $offset, $fecha){
-        return $this -> queryOFFSETDIA('
-            SELECT m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
-            FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto) 
-            WHERE m.eliminado = 0 and p.eliminado=0 and m.fecha = :fecha
-            ORDER BY p.nombre
-            LIMIT :limit  
-            OFFSET :offset', $limit, $offset, $fecha
-        );
+        $this->db->select('m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado');
+        $this->db->from('menu m');
+        $this->db->join('producto p', 'm.idProducto=p.idProducto');
+        $this->db->where('m.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->where('m.fecha', $fecha);
+        $this->db->order_by('p.nombre', 'ASC');
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+
+        /*   return $this -> queryOFFSETDIA('
+               SELECT m.idMenu,m.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
+               FROM menu m INNER JOIN producto p ON(m.idProducto=p.idProducto)
+               WHERE m.eliminado = 0 and p.eliminado=0 and m.fecha = :fecha
+               ORDER BY p.nombre
+               LIMIT :limit
+               OFFSET :offset', $limit, $offset, $fecha
+           );*/
     }
     
     public function actualizarMenu($menu){
-        return $this -> query("
+
+        $data=array('idProducto' => $menu["idProducto"],
+                    'fecha' => $menu["fecha"],
+                    'foto' => $menu["foto"]);
+        $this->db->where('idMenu',$menu['idMenu']);
+        $this->db->update('menu',$data);
+        /*return $this -> query("
             UPDATE menu 
             SET idProducto= :idProducto,
                 fecha= :fecha,
@@ -143,12 +256,17 @@ class MenuModel extends Model{
                 'fecha' => $menu["fecha"],
                 'foto' => $menu["foto"],
                 'idMenu' => $menu["idMenu"])
-        );
+        );*/
     }
 
     public function insertarProd($idMenu, $idProd){
 
-        return $this->query(
+        $data = array(  "idMenu" => $idMenu,
+                        "idProducto" => $idProd);
+
+        $this->db->insert('menuProducto', $data);
+
+        /*return $this->query(
             "INSERT INTO menuProducto(
                 idMenu,
                 idProducto)
@@ -159,13 +277,22 @@ class MenuModel extends Model{
                 "idMenu" => $idMenu,
                 "idProd" => $idProd
             )
-        );
+        );*/
 
     }
 
 
     public function getMenu($limit, $offset, $idMenu){
-        $this->stmnt = $this->db->prepare("
+
+        $this->db->select('m.idMenu,mp.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado');
+        $this->db->from('menuProducto mp');
+        $this->db->join('menu m', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('mp.idMenu', $idMenu);
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+       /* $this->stmnt = $this->db->prepare("
             SELECT m.idMenu,mp.idProducto,m.fecha,m.foto, m.eliminado,p.nombre,p.stock,p.stockMinimo,p.precioVentaUnitario,p.descripcion,p.eliminado
             FROM menuProducto mp
             INNER JOIN menu m ON (mp.idMenu = m.idMenu)
@@ -178,13 +305,20 @@ class MenuModel extends Model{
         $this->stmnt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
         $this->stmnt->bindValue(':idMenu', $idMenu);
         $this->stmnt->execute();
-        return $this->stmnt ->fetchAll();
+        return $this->stmnt ->fetchAll();*/
     }
 
 
      public function insertarMenu($fecha, $foto){
 
-        return $this -> lastId("
+         $data= array(
+             'fecha' =>$fecha,
+             'foto' => $foto,
+             'eliminado' => 0,
+             'habilitado' => 0
+         );
+         $this->db->insert('menu', $data);
+   /*     return $this -> lastId("
             INSERT INTO menu(
                 fecha,
                 foto,
@@ -199,12 +333,19 @@ class MenuModel extends Model{
                 'fecha' =>$fecha,
                 'foto' => $foto
                 )
-        );
+        );*/
     }
 
     public function insertarMenu2($fecha, $foto, $hab){
+        $data= array(
+            'fecha' =>$fecha,
+            'foto' => $foto,
+            'eliminado' => 0,
+            'habilitado' => $hab
+        );
 
-        return $this -> lastId("
+       $this->db->insert('menu', $data);
+        /*return $this -> lastId("
             INSERT INTO menu(
                 fecha,
                 foto,
@@ -220,24 +361,37 @@ class MenuModel extends Model{
                 'foto' => $foto,
                 'hab' => $hab
             )
-        );
+        );*/
     }
 
     public function eliminarMenu($idMenu){
-        return $this -> query(
+
+        $data=array('m.eliminado' => 1);
+        $this->db->where('m.idMenu', $idMenu);
+        $this->db->update('menu m', $data);
+
+     /*   return $this -> query(
             "UPDATE menu m 
             SET m.eliminado =1 
-            WHERE m.idMenu = :idMenu" , array('idMenu' => $idMenu));
+            WHERE m.idMenu = :idMenu" , array('idMenu' => $idMenu)); */
     }
     public function getMinimoMenu($fecha){
-        return $this -> queryPreparadaSQL("
+        $this->db->select('MIN(p.stock) as minimo');
+        $this->db->from('menu m');
+        $this->db->join('menuProducto mp', 'mp.idMenu = m.idMenu');
+        $this->db->join('producto p', 'mp.idProducto = p.idProducto');
+        $this->db->where('m.fecha',$fecha);
+        $this->db->where('m.eliminado',0);
+        $this->db->get()->result();
+
+    /*    return $this -> queryPreparadaSQL("
             SELECT MIN(p.stock) as minimo
             FROM menu m INNER JOIN menuProducto mp ON (mp.idMenu = m.idMenu)
                         INNER JOIN producto p ON (mp.idProducto = p.idProducto)
             WHERE m.fecha = :fecha
             AND m.eliminado = 0
             ", array('fecha' => $fecha));
-       
+       */
     }
 }
 ?>
