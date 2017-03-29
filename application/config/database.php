da356cfdcd16a828ae9474433fb0f9ab87b6f68d
parinisantiago <parinisantiago@gmail.com>
2017-03-29 18:45:41 -0300

    agrego charset utf8 a la configuración

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost; dbname=proyecto;',
+	'dsn'	=> 'mysql:host=localhost; dbname=proyecto; charset=utf8',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'root',
