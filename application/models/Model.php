1ee80ed5995363c6845e0c87a72379c2317be411
parinisantiago <parinisantiago@gmail.com>
2017-03-29 18:45:08 -0300

    extiendo Model de CI_Model y borro la funcionalidad innecesaria

--- application/models/Model.php
+++ application/models/Model.php
@@ -1,54 +1,55 @@
 <?php
 
 include_once(dirname(__DIR__)."/Utils/Const.php");
+include_once(dirname(__DIR__)."/../system/core/Model.php");
+class Model extends CI_Model{
 
-class Model{
-    protected $db;
-    protected $stmnt;
+
     
     public function __construct(){
-        try {
+        parent::__construct();
+        /*try {
             $this->db = new PDO ( "mysql:host=" . host . ";dbname=" . db, user, pass ); //las constantes estan definidas en Utils/const.php
             $this->db->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ ); // para retornar objetos
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Para retornar PDOExecption (creo)
         }catch (PDOException $e) {
-            print "¡Error!: " . $e->getMessage() . "<br/>";
-	}
+            print "¡Error!: " . $e->getMessage() . "<br/>";*/
+
     }
 
     protected function queryPreparadaSQL($sql, $parametros){
-        $this->query($sql, $parametros);
-	    return $this -> stmnt -> fetch(); //retorna el valor de la consulta como un objeto
+       /* $this->query($sql, $parametros);
+	    return $this -> stmnt -> fetch(); //retorna el valor de la consulta como un objeto*/
     }
     protected function queryOFFSET($sql, $limit, $offset){
-        $this->stmnt = $this->db->prepare($sql);
+      /*  $this->stmnt = $this->db->prepare($sql);
         $this->stmnt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
         $this->stmnt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
         $this->stmnt->execute();
-        return $this->stmnt ->fetchAll();
+        return $this->stmnt ->fetchAll();*/
     }
     protected function queryOFFSETDIA($sql, $limit, $offset, $fecha){
-        $this->stmnt = $this->db->prepare($sql);
+       /* $this->stmnt = $this->db->prepare($sql);
         $this->stmnt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
         $this->stmnt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
         $this->stmnt->bindValue(':fecha', $fecha);
         $this->stmnt->execute();
-        return $this->stmnt ->fetchAll();
+        return $this->stmnt ->fetchAll();*/
     }
     protected function queryTodasLasFilas($sql, $parametros){
-        $this->stmnt = $this->db->prepare($sql);
+     /*   $this->stmnt = $this->db->prepare($sql);
         $this->stmnt->execute($parametros);
-        return $this -> stmnt -> fetchAll(); //retorna el valor de la consulta como un objeto
+        return $this -> stmnt -> fetchAll(); //retorna el valor de la consulta como un objeto*/
     }
     protected function query($sql, $parametros){
-        $this->stmnt = $this->db->prepare($sql);
+       /* $this->stmnt = $this->db->prepare($sql);
 	    $this->stmnt->execute($parametros);
-	    return true;
+	    return true;*/
     }
 
     protected function lastId($sql, $parametros){
-        $this->query($sql, $parametros);
-        return $this->db->lastInsertId();
+       /* $this->query($sql, $parametros);
+        return $this->db->lastInsertId();*/
     }
 }
 
