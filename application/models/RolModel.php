edfc9dd003791fca841ee2c89500f2b35c4bf0f1
parinisantiago <parinisantiago@gmail.com>
2017-03-30 14:06:49 -0300

    Integro rolModel con codeigniter

--- application/models/RolModel.php
+++ application/models/RolModel.php
@@ -10,16 +10,27 @@ class RolModel extends Model
 
     public function getAllRols()
     {
-        return $this -> queryTodasLasFilas("SELECT * FROM rol", array());
+        $this->db->select('*');
+        $this->db->from('rol');
+        return $this->db->get()->result();
+       /* return $this -> queryTodasLasFilas("SELECT * FROM rol", array());*/
     }
 
     public function getRolById($id)
     {
-        return $this ->queryPreparadaSQL("SELECT * FROM rol WHERE idRol = :id", array('id' => $id));
+        $this->db->select('*');
+        $this->db->from('rol');
+        $this->db->where('idRol', $id);
+        return $this->db->get()->result();
+       /* return $this ->queryPreparadaSQL("SELECT * FROM rol WHERE idRol = :id", array('id' => $id));*/
     }
 
     public function getAllUbicacion()
     {
-        return $this -> queryTodasLasFilas("SELECT * FROM ubicacion", array());
+        $this->db->select('*');
+        $this->db->from('ubicacion');
+        return $this->db->get()->result();
+
+       /* return $this -> queryTodasLasFilas("SELECT * FROM ubicacion", array());*/
     }
 }
