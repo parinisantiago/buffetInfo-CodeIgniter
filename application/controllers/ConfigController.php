99e1a79006f02fd89bfd93f4101a833c704c7e1d
parinisantiago <parinisantiago@gmail.com>
2017-03-30 13:36:03 -0300

    Adapto ConifController a codeigniter

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -1,34 +1,28 @@
 <?php
-
+include_once('MainController.php');
 
 class ConfigController extends Controller
 {
 
-    public $model;
-    public $rolModel;
-    public $productoModel;
-    public $categoriaModel;
-    public $ventaModel;
-    public $compraModel;
-    public $menuModel;
+
 
     public function __construct(){
         parent::__contruct();
-        $this->model = new MainUserModel();
-        $this->rolModel = new RolModel();
-        $this->productoModel = new ProductosModel();
-        $this->categoriaModel = new CategoriaModel();
-        $this->ventaModel = new VentaModel();
-        $this->compraModel= new CompraModel();
-        $this->menuModel= new MenuModel();
+        $this->load->model('UserModel');
+        $this->load->model('RolModel');
+        $this->load->model('ProductosModel');
+        $this->load->model('CategoriaModel');
+        $this->load->model('VentaModel');
+        $this->load->model('CompraModel');
+        $this->load->model('MenuModel');
     }
 
     public function index(){
-        $this->dispatcher->render("Backend/IndexTemplate.twig");
+        $this->display("IndexTemplate.twig");
     }
 
     public function setMensajeError($error){
-        $this->dispatcher->mensajeError = $error;
+        $this->addData('mensajeError', $error);
         $this->index();
     }
 
@@ -40,28 +34,32 @@ class ConfigController extends Controller
 
     public function configuracionSitio()
     {
-        $this->dispatcher->render("Backend/ConfiguracionTemplate.twig");
+        $this->display("ConfiguracionTemplate.twig");
     }
 
     public function changeConf()
     {
         $this->validarConf();
         $this->conf->updateConf($_POST);
-        $this->dispatcher->config = $this->conf->getConfiguracion();
-        $this->dispatcher->render("Backend/ConfiguracionTemplate.twig");
+        $this->addData('config', $this->conf->getConfiguracion());
+        $this->display("ConfiguracionTemplate.twig");
     }
 
     private function validarConf()
     {
-        if (! isset($_POST['submitButton'])) throw new Exception('Apreta el boton de modificar macho');
-        if (! isset($_POST['titulo'])) throw new Exception('Falta escribir el titulo');
-        elseif (! preg_match("/^[a-zA-Z0-9 ]+$/", $_POST['titulo'])) throw new Exception('Valor del titulo no valido');
-        if (! isset($_POST['descripcion'])) throw new Exception('Falta escribir el descripcion');
-        elseif (! preg_match("/^[a-zA-Z0-9 ;.,àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹ]+$/", $_POST['descripcion'])) throw new Exception('Valor de la  descripcion no valido');
-        if (! isset($_POST['mensaje'])) throw new Exception('Falta escribir el mensaje');
-        elseif (! preg_match("/^[a-zA-Z ]+$/", $_POST['mensaje'])) throw new Exception('Valor del mensaje no valido');
-        if (! isset($_POST['lista'])) throw new Exception('Falta escribir el numero de la lista');
-        elseif (! preg_match("/^[0-9]+$/", $_POST['lista'])) throw new Exception('Valor del numero de la lista no valido');
-
+        try {
+            if (!isset($_POST['submitButton'])) throw new Exception('Apreta el boton de modificar macho');
+            if (!isset($_POST['titulo'])) throw new Exception('Falta escribir el titulo');
+            elseif (!preg_match("/^[a-zA-Z0-9 ]+$/", $_POST['titulo'])) throw new Exception('Valor del titulo no valido');
+            if (!isset($_POST['descripcion'])) throw new Exception('Falta escribir el descripcion');
+            elseif (!preg_match("/^[a-zA-Z0-9 ;.,àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹ]+$/", $_POST['descripcion'])) throw new Exception('Valor de la  descripcion no valido');
+            if (!isset($_POST['mensaje'])) throw new Exception('Falta escribir el mensaje');
+            elseif (!preg_match("/^[a-zA-Z ]+$/", $_POST['mensaje'])) throw new Exception('Valor del mensaje no valido');
+            if (!isset($_POST['lista'])) throw new Exception('Falta escribir el numero de la lista');
+            elseif (!preg_match("/^[0-9]+$/", $_POST['lista'])) throw new Exception('Valor del numero de la lista no valido');
+        } catch (Exception $e){
+            $controller= new MainController();
+            $controller->index();
+        }
     }
 }
