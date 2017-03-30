f26af2477b465f9e747e360ef5557a6e17256d8e
parinisantiago <parinisantiago@gmail.com>
2017-03-30 16:01:46 -0300

    integracion de categoriaModel con codeigniter

--- application/models/CategoriaModel.php
+++ application/models/CategoriaModel.php
@@ -7,11 +7,20 @@ class CategoriaModel extends Model{
     }
 
     public function getCategoriaById($id){
-        return $this -> queryPreparadaSQL('SELECT * FROM categoria WHERE idCategoria = :id', array('id' => $id));
+        $this->db->select('*');
+        $this->db->from('categoria');
+        $this->db->where('idCategoria', $id);
+        return $this->db->get()->result();
+
+        /*return $this -> queryPreparadaSQL('SELECT * FROM categoria WHERE idCategoria = :id', array('id' => $id));*/
     }
 
     public function getAllCategorias(){
-        return $this -> queryTodasLasFilas('SELECT * FROM categoria c', array());
+        $this->db->select('*');
+        $this->db->from('categoria');
+        return $this->db->get()->result();
+
+        /*return $this -> queryTodasLasFilas('SELECT * FROM categoria c', array());*/
     }
     
 }  
