2b6f0690e93494409511db6cc4517655bc0f7b12
parinisantiago <parinisantiago@gmail.com>
2017-03-30 18:47:32 -0300

    integro CompraModel a CodeIgniter

--- application/models/CompraModel.php
+++ application/models/CompraModel.php
@@ -7,47 +7,105 @@ class CompraModel extends Model{
         parent::__construct();
     }
     public function totalCompras(){
-        return $this->queryPreparadaSQL('SELECT COUNT(*) AS total  FROM compra c INNER JOIN proveedor pr ON(c.idProveedor=pr.idProveedor)INNER JOIN producto p ON (p.idProducto=c.idProducto) 
+
+        $this->db->select('COUNT(*) AS total');
+        $this->db->form('compra c');
+        $this->db->join('proveedor pr', 'c.idProveedor=pr.idProveedor');
+        $this->db->join('producto p', 'p.idProducto=c.idProducto');
+        $this->db->where('c.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->order_by('c.idCompra');
+        $total = $this->db->get()->result();
+        return $total[0];
+
+        /*r0eturn $this->queryPreparadaSQL('SELECT COUNT(*) AS total  FROM compra c INNER JOIN proveedor pr ON(c.idProveedor=pr.idProveedor)INNER JOIN producto p ON (p.idProducto=c.idProducto)
             WHERE c.eliminado = 0 and p.eliminado=0
-            ORDER BY c.idCompra', array());
+            ORDER BY c.idCompra', array());*/
     }
     public function getAllCompras($limit, $offset){
-        return $this -> queryOFFSET('
+        $this->db->select('c.idCompra, p.nombre, p.marca, c.cantidad, c.precioUnitario, pr.proveedor, c.fecha, c.fotoFactura');
+        $this->db->form('compra c');
+        $this->db->join('proveedor pr', 'c.idProveedor=pr.idProveedor');
+        $this->db->join('producto p', 'p.idProducto=c.idProducto');
+        $this->db->where('c.eliminado', 0);
+        $this->db->where('p.eliminado', 0);
+        $this->db->order_by('c.idCompra');
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        return $this->db->get()->result();
+       /* return $this -> queryOFFSET('
             SELECT c.idCompra, p.nombre, p.marca, c.cantidad, c.precioUnitario, pr.proveedor, c.fecha, c.fotoFactura
             FROM compra c INNER JOIN proveedor pr ON(c.idProveedor=pr.idProveedor)INNER JOIN producto p ON (p.idProducto=c.idProducto) 
             WHERE c.eliminado = 0 and p.eliminado=0
             ORDER BY c.idCompra
             LIMIT :limit  
             OFFSET :offset', $limit, $offset
-        );
+        );*/
     }
      public function siguienteId(){
-        return $this -> queryPreparadaSQL('SELECT MAX(idCompra) as idCompra FROM compra',array());
+         $this->db->select('MAX(idCompra) as idCompra');
+         $this->db->form('compra');
+         $total = $this->db->get()->result();
+         return $total[0];
+
+         /*return $this -> queryPreparadaSQL('SELECT MAX(idCompra) as idCompra FROM compra',array());*/
     }
      public function searchIdCompra($idComp){
-        return $this ->queryPreparadaSQL("
+         $this->db->select('c.idCompra,c.idProducto,c.idProveedor, p.nombre, p.marca, c.cantidad, c.precioUnitario, pr.proveedor, c.fecha,c.fotoFactura');
+         $this->db->form('compra c');
+         $this->db->join('proveedor pr', 'c.idProveedor=pr.idProveedor');
+         $this->db->join('producto p', 'p.idProducto=c.idProducto');
+         $this->db->where('c.eliminado', 0);
+         $this->db->where('p.eliminado', 0);
+         $this->db->where('c.idCompra', $idComp);
+         return $this->db->get()->result();
+      /*   return $this ->queryPreparadaSQL("
             SELECT c.idCompra,c.idProducto,c.idProveedor, p.nombre, p.marca, c.cantidad, c.precioUnitario, pr.proveedor, c.fecha,c.fotoFactura
             FROM compra c INNER JOIN proveedor pr ON(c.idProveedor=pr.idProveedor)INNER JOIN producto p ON (p.idProducto=c.idProducto) 
-            WHERE c.eliminado = 0 and p.eliminado=0 and c.idCompra = :idComp" , array('idComp' => $idComp));
+            WHERE c.eliminado = 0 and p.eliminado=0 and c.idCompra = :idComp" , array('idComp' => $idComp));*/
     }
     
     public function searchIdProveedor($idProv){
-        return $this ->queryPreparadaSQL("
+        $this->db->select('pr.idProveedor');
+        $this->db->form('proveedor pr');
+        $this->db->where('pr.idProveedor', $idProv);
+        return $this->db->get()->result();
+
+       /* return $this ->queryPreparadaSQL("
             SELECT pr.idProveedor
             FROM proveedor pr 
-            WHERE  pr.idProveedor = :idProv" , array('idProv' => $idProv));
+            WHERE  pr.idProveedor = :idProv" , array('idProv' => $idProv));*/
     }
     public function getAllProveedor($limit, $offset){
-        return $this -> queryOFFSET('
+
+        $this->db->select('pr.idProveedor, pr.proveedor, pr.proveedorCuit');
+        $this->db->form('proveedor pr');
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        return $this->db->get()->result();
+
+      /*  return $this -> queryOFFSET('
             SELECT pr.idProveedor, pr.proveedor, pr.proveedorCuit
             FROM proveedor pr
             LIMIT :limit  
             OFFSET :offset', $limit, $offset
-            );
+            );*/
     }
     
     public function actualizarCompra($comp){
-        return $this -> query("
+
+        $data = array(
+            'idProducto' => $comp["producto"],
+            'cantidad' => $comp["cantidad"],
+            'precioUnitario' => $comp["precioUnitario"],
+            'idProveedor' => $comp["proveedor"],
+            'fecha' => $comp["fecha"],
+            'fotoFactura' => $comp["fotoFactura"]
+        );
+        $this->db->where('idCompra', $comp["idCompra"]);
+        $this->db->update('compra', $data);
+
+ /*       return $this -> query("
             UPDATE compra 
             SET idProducto= :idProducto,
                 cantidad= :cantidad,
@@ -63,12 +121,22 @@ class CompraModel extends Model{
                 'idProveedor' => $comp["proveedor"],
                 'fecha' => $comp["fecha"],
                 'fotoFactura' => $comp["fotoFactura"])
-        );
+        );*/
     }
     
      public function insertarCompra($comp){
         $today=getDate();
-        return $this -> query("
+        $data = array(
+            'idProducto' => $comp["producto"],
+            'cantidad' => $comp["cantidad"],
+            'precioUnitario' => $comp["precioUnitario"],
+            'idProveedor' => $comp["proveedor"],
+            'fecha' =>$today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'],
+            'eliminado' => 0,
+            'fotoFactura' => $comp["fotoFactura"]
+        );
+        $this->db->insert('compra', $data);
+      /*  return $this -> query("
             INSERT INTO compra(
                 idProducto,
                 cantidad,
@@ -91,14 +159,18 @@ class CompraModel extends Model{
                 'fecha' =>$today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'],
                 'fotoFactura' => $comp["fotoFactura"]
                 )
-        );
+        );*/
     }
-     
+
     public function eliminarCompra($idCompra){
-        return $this -> query(
+
+        $data = array('eliminado' => 1);
+        $this->db->where('idCompra',$idCompra);
+        $this->db->update('compra', $data);
+       /* return $this -> query(
             "UPDATE compra c 
             SET c.eliminado =1 
-            WHERE c.idCompra = :idCompra" , array('idCompra' => $idCompra));
+            WHERE c.idCompra = :idCompra" , array('idCompra' => $idCompra));*/
     }
 }
 ?>
