4230d98266d27cf4c7f202b401537d77869c289b
parinisantiago <parinisantiago@gmail.com>
2017-03-31 10:14:37 -0300

    Integro balanceModel a codeigniter

--- application/models/BalanceModel.php
+++ application/models/BalanceModel.php
@@ -11,64 +11,111 @@ class BalanceModel extends Model
 
     function ingresoDia($fecha)
     {
-        return $this->queryPreparadaSQL("
+        $this->db->select('ROUND(SUM(precioUnitario * cantidad), 2) AS total');
+        $this->db->from('ingresoDetalle');
+        $this->db->where('fecha', $fecha);
+        $this->db->where('eliminado', 0);
+        $total = $this->db->get()->result();
+        return $total[0];
+
+    /*   return $this->queryPreparadaSQL("
             SELECT ROUND(SUM(precioUnitario * cantidad), 2) AS total
             FROM ingresoDetalle
             WHERE fecha=:fecha AND eliminado = 0",
             array("fecha" => $fecha)
-        );
+        );*/
     }
 
     function egresoDia($fecha)
     {
-        return $this->queryPreparadaSQL("
+        $this->db->select('ROUND(SUM(precioUnitario * cantidad), 2) AS total');
+        $this->db->from('compra');
+        $this->db->where('fecha', $fecha);
+        $this->db->where('eliminado', 0);
+        $total = $this->db->get()->result();
+        return $total[0];
+
+      /*  return $this->queryPreparadaSQL("
             SELECT ROUND(SUM(precioUnitario * cantidad), 2) AS total
             FROM compra
             WHERE fecha=:fecha AND eliminado = 0",
             array("fecha" => $fecha)
-        );
+        );*/
     }
 
 
     function ingresoRango($fechaInicio, $fechaFin)
     {
-        return $this->queryTodasLasFilas("
+        $this->db->select('ROUND(SUM(precioUnitario * cantidad), 2) AS total, fecha');
+        $this->db->from('ingresoDetalle');
+        $this->db->where('eliminado', 0);
+        $this->db->where('fecha >=', $fechaInicio);
+        $this->db->where('fecha <=', $fechaFin);
+        $this->db->group_by('fecha');
+        $total = $this->db->get()->result();
+        return $total[0];
+
+/*        return $this->queryTodasLasFilas("
             SELECT ROUND(SUM(precioUnitario * cantidad), 2) AS total, fecha
             FROM ingresoDetalle
             WHERE eliminado = 0
             AND fecha BETWEEN :fechaInicio AND :fechaFin
             GROUP BY fecha",
             array("fechaInicio" => $fechaInicio, "fechaFin" => $fechaFin)
-        );
+        );*/
     }
 
     function egresoRango($fechaInicio, $fechaFin)
     {
-        return $this->queryTodasLasFilas("
+        $this->db->select('ROUND(SUM(precioUnitario * cantidad), 2) AS total, fecha');
+        $this->db->from('compra');
+        $this->db->where('eliminado', 0);
+        $this->db->where('fecha >=', $fechaInicio);
+        $this->db->where('fecha <=', $fechaFin);
+        $this->db->group_by('fecha');
+        $total = $this->db->get()->result();
+        return $total[0];
+
+    /*    return $this->queryTodasLasFilas("
             SELECT ROUND(SUM(precioUnitario * cantidad), 2) AS total, fecha
             FROM compra
             WHERE eliminado = 0
             AND fecha BETWEEN :fechaInicio AND :fechaFin
             GROUP BY fecha",
             array("fechaInicio" => $fechaInicio, "fechaFin" => $fechaFin)
-        );
+        );*/
     }
 
     function productosEgresoDia($fecha)
     {
-        return $this->queryTodasLasFilas("
+        $this->db->select('cantidad AS cant, p.nombre');
+        $this->db->from('ingresoDetalle i');
+        $this->db->join('producto p', 'p.idProducto = i.idProducto');
+        $this->db->where('i.fecha', $fecha);
+        $this->db->where('i.eliminado', 0);
+        $this->db->group_by('i.idProducto');
+        return $this->db->get()->result();
+     /*   return $this->queryTodasLasFilas("
             SELECT cantidad AS cant, p.nombre
             FROM ingresoDetalle i
             INNER JOIN producto p 
             ON (p.idProducto = i.idProducto)
             WHERE i.fecha =:fecha AND i.eliminado = 0
             GROUP BY i.idProducto",
-            array("fecha" => $fecha));
+            array("fecha" => $fecha));*/
     }
 
     function productosEgresoRango($fechaInicio, $fechaFin)
     {
-        return $this->queryTodasLasFilas("
+        $this->db->select('SUM(cantidad) AS cant, p.nombre');
+        $this->db->from('ingresoDetalle i');
+        $this->db->join('producto p', 'p.idProducto = i.idProducto');
+        $this->db->where('i.fecha >=', $fechaInicio);
+        $this->db->where('i.fecha <=', $fechaFin);
+        $this->db->where('i.eliminado', 0);
+        $this->db->group_by('i.idProducto');
+        return $this->db->get()->result();
+       /* return $this->queryTodasLasFilas("
             SELECT SUM(cantidad) AS cant, p.nombre
             FROM ingresoDetalle i
             INNER JOIN producto p 
@@ -76,6 +123,6 @@ class BalanceModel extends Model
             WHERE i.fecha BETWEEN :fechaInicio AND :fechaFin
             AND i.eliminado = 0
             GROUP BY i.idProducto",
-            array("fechaInicio" => $fechaInicio, "fechaFin" => $fechaFin));
+            array("fechaInicio" => $fechaInicio, "fechaFin" => $fechaFin));*/
     }
 }
