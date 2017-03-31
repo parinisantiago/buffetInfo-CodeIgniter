8db8de64816e2f1a50a56280d0c253aa252a4b78
parinisantiago <parinisantiago@gmail.com>
2017-03-31 11:12:28 -0300

    integro CompraController con CodeIgniter

--- application/controllers/ComprarController.php
+++ application/controllers/ComprarController.php
@@ -1,15 +1,14 @@
 <?php
 
-require_once 'Controller/Controller.php';
+require_once 'Controller.php';
 
 class ComprarController extends Controller{
-    public $productoModel;
-    public $compraModel;
+
   
     public function __construct(){
-            parent::__contruct();
-            $this->productoModel = new ProductosModel();
-            $this->compraModel= new CompraModel(); 
+            parent::__construct();
+            $this->load->model('ProductosModel');
+            $this->load->model('CompraModel');
     }
 
     public function getPermission()
@@ -20,24 +19,24 @@ class ComprarController extends Controller{
     }
 
     public function compraListar(){
-        $this->paginaCorrecta($this->compraModel->totalCompras());
-        $this->dispatcher->compras = $this->compraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']);
-        $this->dispatcher->pag = $_GET['pag'];
-        $this->dispatcher->method = "compraListar";
-        $this->dispatcher->render("Backend/CompraListarTemplate.twig");
+        $this->paginaCorrecta($this->CompraModel->totalCompras());
+        $this->addData('compras', $this->CompraModel->getAllCompras($this->conf->getConfiguracion()->cantPagina,$_GET['offset']));
+        $this->addData('pag', $_GET['pag']);
+        $this->addData('method', "compraListar");
+        $this->display("CompraListarTemplate.twig");
     }
     public function compraAM(){
         if (isset($_POST["idCompra"])){ //modificar una compra
-            $this->dispatcher->compra =$this ->compraModel->searchIdCompra($_POST["idCompra"]);
+            $this->addData('compra', $this->CompraModel->searchIdCompra($_POST["idCompra"]));
         }//crear una compra nueva
-        $this->dispatcher->proveedor =$this ->compraModel->getAllProveedor(99999,0);
-        $this->dispatcher->productos =$this ->productoModel->getProductos();
-        $this->dispatcher->render("Backend/CompraAMTemplate.twig");
+        $this->addData('proveedor', $this->CompraModel->getAllProveedor(99999,0));
+        $this->addData('productos', $this ->ProductosModel->getProductos());
+        $this->display("CompraAMTemplate.twig");
     }
     public function compraAMPost(){
         try{
         $this->validarCompra($_POST);
-        $producto = $this->productoModel->searchIdProducto($_POST['producto']);
+        $producto = $this->ProductosModel->searchIdProducto($_POST['producto']);
         if ($_FILES['uploadedfile']['size'] >1){//si hay foto de la factura  !isset($_POST['fotoFactura']
             $this->subirFoto();
         }else{//pregunto si ya hay una foto
@@ -46,38 +45,38 @@ class ComprarController extends Controller{
             }
         }
         if ($_POST["idCompra"] != ""){
-           $compra =  $this->compraModel->searchIdCompra($_POST['idCompra']);
+           $compra =  $this->CompraModel->searchIdCompra($_POST['idCompra']);
            $nuevoStock = $producto->stock - $compra->cantidad + $_POST['cantidad'];
             if ($nuevoStock < 0) throw new Exception("No se puede modificar la compra porque quedaria sin stock el producto");
-            $this->productoModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
-            $this->dispatcher->compra =$this->compraModel->actualizarCompra($_POST);
+            $this->ProductosModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
+            $this->addData('compra', $this->CompraModel->actualizarCompra($_POST));
         }else{
-            $this->dispatcher->compra =$this ->compraModel->insertarCompra($_POST);
+            $this->addData('compra', $this ->CompraModel->insertarCompra($_POST));
             $nuevoStock = $producto -> stock + $_POST['cantidad'];
             $nuevoStock .= "";
-            $this->productoModel->actualizarCantProductos($_POST['producto'], $nuevoStock);
+            $this->ProductosModel->actualizarCantProductos($_POST['producto'], $nuevoStock);
         }
         $_GET['pag'] = 0;
-        $this->dispatcher->method = "CompraListar";
+        $this->addData('method', "CompraListar");
         $this->compraListar();
-    } catch (valException $e){
-        $this->dispatcher->ventas = $_POST;
-        $this->dispatcher->mensajeError = $e->getMessage();
+    } catch (Exception $e){
+        $this->addData('ventas', $_POST);
+        $this->addData('mensajeError', $e->getMessage());
         $this->compraAM();
 }
     }
     public function compraEliminar(){
         $this->validator->varSet($_POST['idCompra'], "Error: no existe la compra");
         $this->validator->varSet($_POST['submitButton'], "Presione el boton de submit");
-        $compra = $this->compraModel->searchIdCompra($_POST['idCompra']);
-        $producto = $this->productoModel->searchIdProducto($compra->idProducto);
+        $compra = $this->CompraModel->searchIdCompra($_POST['idCompra']);
+        $producto = $this->ProductosModel->searchIdProducto($compra->idProducto);
         $nuevoStock = $producto -> stock - $compra -> cantidad;
         if ($nuevoStock < 0) throw new Exception("Error: no se puede eliminar la compra, ya que se han vendido productos");
         $nuevoStock .= "";
-        $this->productoModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
-        $this->dispatcher->compra =$this ->compraModel->eliminarCompra($compra->idCompra);
+        $this->ProductosModel->actualizarCantProductos($producto->idProducto, $nuevoStock);
+        $this->addData('compra', $this ->CompraModel->eliminarCompra($compra->idCompra));
         $_GET['pag'] = 0;
-        $this->dispatcher->method = "CompraListar";
+        $this->addData('method', "CompraListar");
         $this->compraListar();
     }
     public function subirFoto(){
@@ -91,8 +90,8 @@ class ComprarController extends Controller{
     } 
     public function validarCompra($var){
         $this->validator->varSet($var['submit'],"Presione el boton de submit");
-        if (! $this->compraModel->searchIdProveedor($var['proveedor'])) throw new valException("Error: No existe el proveedor");
-        if (! $this->productoModel->searchIdProducto($var['producto'])) throw new valException("Error: No existe el producto");
+        if (! $this->CompraModel->searchIdProveedor($var['proveedor'])) throw new valException("Error: No existe el proveedor");
+        if (! $this->ProductosModel->searchIdProducto($var['producto'])) throw new valException("Error: No existe el producto");
         $this->validator->validarNumeros($var['cantidad'],"Error: error en cantidad",5);
         $this->validator->validarNumerosPunto($var['precioUnitario'],"Error: error en precio unitario",50);
     }
