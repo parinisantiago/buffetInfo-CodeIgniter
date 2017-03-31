55357ce9991ad451a1a99dcb425b11de5683640f
parinisantiago <parinisantiago@gmail.com>
2017-03-31 10:45:23 -0300

    integro VentaModel a CodeIgniter

--- application/models/VentaModel.php
+++ application/models/VentaModel.php
@@ -8,47 +8,81 @@ class VentaModel extends Model{
     /******OJO YA LISTA LOS NO ELIMINADOS, SACARLO LA PREGUNTA DE TWIG*/
 
     public function totalVenta(){
-        return $this->queryPreparadaSQL('
+
+        $this->db->select('COUNT(*) AS total');
+        $this->db->from('producto p');
+        $this->db->join('ingresoDetalle i', 'p.idProducto=i.idProducto');
+        $this->db->where('i.eliminado', 0);
+        $this->db->get()->result();
+
+       /* return $this->queryPreparadaSQL('
             SELECT COUNT(*) AS total 
             FROM producto p 
             INNER JOIN ingresoDetalle i ON (p.idProducto=i.idProducto) 
             WHERE i.eliminado = 0 '
-            ,array());
+            ,array());*/
     }
 
     public function getVentaById($id){
-        return $this->queryPreparadaSQL('
+
+        $this->db->select('*');
+        $this->db->from('ingresoDetalle i');
+        $this->db->where('i.idIngresoDetalle', $id);
+        $this->db->get()->result();
+    /*    return $this->queryPreparadaSQL('
             SELECT * 
             FROM ingresoDetalle i 
             WHERE i.idIngresoDetalle = :id',
-            array("id" => $id));
+            array("id" => $id));*/
     }
 
     public function getAllVenta($limit, $offset){
-        return $this -> queryOFFSET('
+        $this->db->select('i.idIngresoDetalle ,p.idProducto, p.nombre, p.marca,i.cantidad, i.precioUnitario,i.descripcion, i.fecha');
+        $this->db->from('producto p');
+        $this->db->join('ingresoDetalle i', 'p.idProducto=i.idProducto');
+        $this->db->where('i.eliminado', 0);
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $this->db->get()->result();
+     /*   return $this -> queryOFFSET('
             SELECT i.idIngresoDetalle ,p.idProducto, p.nombre, p.marca,i.cantidad, i.precioUnitario,i.descripcion, i.fecha
             FROM producto p
             INNER JOIN ingresoDetalle i ON (p.idProducto=i.idProducto)
             WHERE i.eliminado = 0 
             LIMIT :limit  
             OFFSET :offset', $limit, $offset
-        );
+        );*/
     }
 
     public function getAlltotales(){
-        return $this -> queryTodasLasFilas('
+        $this->db->select('COUNT(i.idProducto) AS total,ROUND(SUM(i.precioUnitario * i.cantidad), 2) AS cant,SUM(i.cantidad) AS vent, i.idProducto, p.nombre ');
+        $this->db->from('ingresoDetalle i');
+        $this->db->join('producto p', 'i.idProducto = p.idProducto');
+        $this->db->group_by('p.idProducto');
+        $this->db->get()->result();
+        /*return $this -> queryTodasLasFilas('
             SELECT COUNT(i.idProducto) AS total,ROUND(SUM(i.precioUnitario * i.cantidad), 2) AS cant,SUM(i.cantidad) AS vent, i.idProducto, p.nombre 
             FROM ingresoDetalle i 
             INNER JOIN producto p 
             ON (i.idProducto = p.idProducto) 
-            GROUP BY p.idProducto',array());
+            GROUP BY p.idProducto',array());*/
     }
     
     public function insertarVenta($vent){
 /*actualiza solo la tabla de vetnas, tambien hay qye hacer la actualizcion en 
  * la tabla de productos*/
         $today=getDate();
-        return $this -> query("
+        $data= array(
+            'idProducto' => $vent["idProducto"],
+            'cantidad' => $vent["cant"],
+            'precioUnitario' => $vent["precioVentaUnitario"],
+            'descripcion' =>  '',
+            'fecha' => $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'],
+            'eliminado' => 0
+        );
+        $this->db->insert('ingresoDetalle', $data);
+
+/*        return $this -> query("
             INSERT INTO ingresoDetalle(
                 idProducto,
                 cantidad,
@@ -68,13 +102,23 @@ class VentaModel extends Model{
                 'descripcion' =>  '',
                 'fecha' => $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds']
                 )
-        );
+        );*/
     }
 
     public function insertarVentaId($vent)
     {
         $today=getDate();
-        return $this -> lastId("
+        $data= array(
+            'idProducto' => $vent["idProducto"],
+            'cantidad' => $vent["cant"],
+            'precioUnitario' => $vent["precioVentaUnitario"],
+            'descripcion' =>  '',
+            'fecha' => $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'],
+            'eliminado' => 0
+        );
+        $this->db->insert('ingresoDetalle', $data);
+        return $this->db->insert_id();
+      /*  return $this -> lastId("
             INSERT INTO ingresoDetalle(
                 idProducto,
                 cantidad,
@@ -94,23 +138,36 @@ class VentaModel extends Model{
                 'descripcion' =>  '',
                 'fecha' => $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds']
             )
-        );
+        );*/
     }
     public function actualizarVenta($cantidad, $id){
 
-        return $this -> query("
+        $data = array(
+            'cantidad' => $cantidad
+        );
+
+        $this->db->where('idIngresoDetalle', $id);
+        $this->db->update('ingresoDetalle', $data);
+       /* return $this -> query("
             UPDATE ingresoDetalle 
             SET cantidad = :cantidad
             WHERE idIngresoDetalle = :id ",
             array('cantidad' => $cantidad,
                 'id' => $id)
-                );
+                );*/
     }
     public function eliminarVenta($idVenta){
-        return $this -> query(
+        $data = array(
+            'eliminado' => 1
+        );
+
+        $this->db->where('idIngresoDetalle', $idVenta);
+        $this->db->update('ingresoDetalle', $data);
+
+ /*       return $this -> query(
             "UPDATE ingresoDetalle id
             SET id.eliminado =1 
-            WHERE id.idIngresoDetalle = :idIngresoDetalle" , array('idIngresoDetalle' => $idVenta));
+            WHERE id.idIngresoDetalle = :idIngresoDetalle" , array('idIngresoDetalle' => $idVenta));*/
     }
 
 
