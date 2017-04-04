ce77506cf6c9d89e1c93eca8d25de76378d368e2
parinisantiago <parinisantiago@gmail.com>
2017-04-04 08:48:07 -0300

    redefino variable files

--- application/Utils/Const.php
+++ application/Utils/Const.php
@@ -3,5 +3,5 @@ define("host", "localhost");
 define("user", "root");
 define("pass", "1234");
 define("db", "grupo64");
-define("files", "uploads/");
+define("files", dirname(dirname(__DIR__))."/public/uploads/");
 ?>
