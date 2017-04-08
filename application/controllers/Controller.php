61aba24c93085dc611f9043967374bad0325dc46
parinisantiago <parinisantiago@gmail.com>
2017-04-08 11:43:52 -0300

    cambio funcion de permisos para que muestre el error correspondiente

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -4,29 +4,36 @@
 
 include_once(dirname(__DIR__)."/Utils/Validador.php");
 include_once(dirname(__DIR__) . "/libraries/Session.php");
+include_once(dirname(__DIR__) . "/models/UserModel.php");
+include_once(dirname(__DIR__)."/models/MenuModel.php");
 
 class Controller extends CI_controller
 {
     protected $validator;
     protected $data;
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         Session::init();
         $this->validator = new Validador();
         $this->conf = new ConfiguracionModel();
         $this->rol();
+        $this->load->model('MenuModel');
     }
 
-    public function getConfig(){
-	return $this->conf->getConfiguracion();
+    public function getConfig()
+    {
+	    return $this->conf->getConfiguracion();
     }
 
-    public function getPermission(){
+    public function getPermission()
+    {
         return (true);
     }
 
-    public function getRol(){
+    public function getRol()
+    {
         return Session::getValue('rol');
     }
 
@@ -36,17 +43,18 @@ class Controller extends CI_controller
         else  $this->data['rol'] = "NaN";
     }
 
-    protected function display($view){
+    protected function display($view)
+    {
         $this->load->library('twig');
         $this->addData('config',$this->getConfig());
         $this->twig->render($view,$this->data);
     }
 
-    protected function token(){
+    protected function token()
+    {
         $token = md5(uniqid(rand(), TRUE));
         Session::setValue($token, 'tokenScrf');
         $this->data['tokenScrf'] = Session::getValue('tokenScrf');
-
     }
 
     protected function tokenIsValid($token)
@@ -68,9 +76,16 @@ class Controller extends CI_controller
         }
         catch (Exception $e)
         {
-            $this->addData('mensajeError', $e->getMessage());
-            $main = new MainController();
-            $main->index();
+            $this->addData('mensajeError', "El usuario actual no posee permisos para realizar esa operación");
+            if( Session::userLogged() )
+            {
+                $this->display("IndexTemplate.twig");
+            }
+            else
+            {
+                $this->addData('menu', $this->MenuModel->getMenuByDia2(NULL, NULL, NULL));
+                $this->display('MainTemplate.twig');
+            }
         }
     }
 }
