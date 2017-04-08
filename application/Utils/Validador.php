24b18d3126854e61ef90cb3df5b5d7f0be0406e2
parinisantiago <parinisantiago@gmail.com>
2017-04-08 11:10:26 -0300

    borro las valExepcions

--- application/Utils/Validador.php
+++ application/Utils/Validador.php
@@ -5,41 +5,41 @@ require_once 'valException.php';
 class Validador{
     public function validarString( $var, $error, $tam ){
         if (!$this->varSet($var, $error) || !$this->tam($var, $tam) || !preg_match("/^[a-zA-Z0-9]+$/", $var) )
-            throw new valException($error);
+            throw new Exception($error);
     }
 
     public function validarStringEspeciales($var, $error, $tam){
         if ( !$this->tam($var, $tam) || !$this->varSet($var, $error) || !preg_match("/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹ ]+$/", $var))
-            throw new valException($error);
+            throw new Exception($error);
     }
     public function validarNumeros($var, $error, $tam){
         if (!$this->tam($var, $tam) || !$this->varSet($var, $error) || !preg_match("/^[0-9]+$/", $var))
-            throw new valException($error);
+            throw new Exception($error);
     }
 
     public function validarFecha($var, $error){
         
         if (!$this->tam($var, 25) || !$this->varSet($var, $error) ||  !preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $var))
-            throw new valException($error);
+            throw new Exception($error);
     }
 
     public function validarMail($var, $error, $tam){
         if (!$this->tam($var, $tam) || !$this->varSet($var, $error) ||  !preg_match("/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/", $var))
-            throw new valException($error);
+            throw new Exception($error);
     }
     
     public function validarNumerosPunto($var, $error, $tam){
         if (!$this->tam($var, $tam) || !$this->varSet($var, $error) || !preg_match("/^[0-9\.0-9]+$/", $var))
-            throw new valException($error);
+            throw new Exception($error);
     }
     public function varSet($var, $error){
-        if (!isset($var)) throw new valException($error);
+        if (!isset($var)) throw new Exception($error);
         return true;
     }
 
     public function tam($var, $tam){
-        if (0 >= strlen($var)) throw new valException("Deben completarse todos los campos");
-        if (  strlen($var) > $tam ) throw new valException($var." no es de un tamaño permitido");
+        if (0 >= strlen($var)) throw new Exception("Deben completarse todos los campos");
+        if (  strlen($var) > $tam ) throw new Exception($var." no es de un tamaño permitido");
         return true;
     }
 }
