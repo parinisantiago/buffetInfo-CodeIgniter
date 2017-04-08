90cd1dd14e0bda078d0762e153e868330b5de096
parinisantiago <parinisantiago@gmail.com>
2017-04-08 11:26:06 -0300

    formateo de codigo

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -6,22 +6,23 @@ include_once(dirname(__DIR__).'/controllers/MainController.php');
 class LoginController extends Controller
 {
 
-    public function __construct(){
-
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('UserModel');
-
     }
 
-    public function index(){
+    public function index()
+    {
         try
         {
-            if (! Session::userLogged()) {
+            if (! Session::userLogged())
+            {
                 $this->validateLogin();
                 $this->user = $this->UserModel->getUser($_POST['username'], $_POST['pass']);
                 $this->setSession();
             }
-        $this->display('IndexTemplate.twig');
+            $this->display('IndexTemplate.twig');
         }
         catch (Exception $e)
         {
@@ -38,24 +39,16 @@ class LoginController extends Controller
         Session::setValue( $this-> user[0] -> idUsuario, 'idUsuario');
         Session::setValue(true, 'logged');
         $this->addData('rol', Session::getValue('rol'));
-
     }
 
     public function validateLogin()
     {
-
         $this->validator->varSet($_POST['submit'], "Presione el boton de submit");
-
         $this->validator->validarString($_POST['username'], "Error: En usuario o contraseña", 15);
         if ($this->UserModel->isDeleted($_POST['username'])) throw new Exception("El usuario a sido eliminado");
-
         $this->validator->validarString($_POST['pass'], "Error: En usuario o contraseña", 15);
-
         if (!$this->UserModel->userExist($_POST['username'])) throw new Exception("Error: El usuario no existe");
-
-
         elseif (!$this->UserModel->passDontMissmatch($_POST['pass'])) throw new Exception("Contraseña incorrecta");
-
     }
 
 
