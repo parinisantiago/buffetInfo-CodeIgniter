7d85f0c7f9912febff2653dfa153354679f053e7
parinisantiago <parinisantiago@gmail.com>
2017-04-08 23:11:29 -0300

    control de permisos para GestionPedidoController

--- application/controllers/GestionPedidoController.php
+++ application/controllers/GestionPedidoController.php
@@ -11,20 +11,25 @@ class GestionPedidoController extends Controller
         $this->load->model('VentaModel');
     }
 
-    public function getPermission(){
+    public function getPermission()
+    {
         Session::init();
         return ((Session::getValue('rol') == '1' ) || (Session::getValue('rol') == '0' ) );
     }
 
-    public function mostrarPedidos(){
-        $_GET['pag'] = 0;
-        $this->paginaCorrecta($this->PedidosModel->totalPedidosPendientes());
-        $this->addData('pedidos', $this->PedidosModel->getPedidosPendientes($this->conf->getConfiguracion()->cantPagina, $_GET['offset']));
-        $this->display('GestionPedidosListarTemplate.twig');
+    public function mostrarPedidos()
+    {
+        if($this->permissions())
+        {
+            $_GET['pag'] = 0;
+            $this->paginaCorrecta($this->PedidosModel->totalPedidosPendientes());
+            $this->addData('pedidos', $this->PedidosModel->getPedidosPendientes($this->conf->getConfiguracion()->cantPagina, $_GET['offset']));
+            $this->display('GestionPedidosListarTemplate.twig');
+        }
     }
 
-    public function paginaCorrecta($total){
-
+    public function paginaCorrecta($total)
+    {
         if (! isset($_GET['pag'])) throw new Exception('Error:No hay una página que mostrar');
         elseif ($total->total <= $_GET['pag'] *  $this->conf->getConfiguracion()->cantPagina){  $_GET['pag'] = 0; $_GET['offset'] = 0;}
         else $_GET['offset'] = $this->conf->getConfiguracion()->cantPagina * $_GET['pag'];
@@ -34,117 +39,125 @@ class GestionPedidoController extends Controller
 
     public function verPedidos()
     {
-        $this->paginaCorrecta($this->PedidosModel->totalPedidosPendientes());
-        $this->addData('pedidos', $this->PedidosModel->getPedidosPendientes($this->conf->getConfiguracion()->cantPagina, $_GET['offset']));
-        $this->addData('pag', $_GET['pag']);
-        $this->display('GestionPedidosListarTemplate.twig');
+        if($this->permissions())
+        {
+            $this->paginaCorrecta($this->PedidosModel->totalPedidosPendientes());
+            $this->addData('pedidos', $this->PedidosModel->getPedidosPendientes($this->conf->getConfiguracion()->cantPagina, $_GET['offset']));
+            $this->addData('pag', $_GET['pag']);
+            $this->display('GestionPedidosListarTemplate.twig');
+        }
     }
 
 
     public function verDetalle()
     {
-        try
+        if($this->permissions())
         {
-            $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?",3);
-            $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
-            if(!$pedido) throw new valException('El pedido no es valido');
-
-            $this->addData('detalles', $this->PedidosModel->getDetalle($_POST['idPedido']));
-            $this->display("mostrarDetalle.twig");
+            try
+            {
+                $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?", 3);
+                $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
+                if (!$pedido) throw new valException('El pedido no es valido');
+                $this->addData('detalles', $this->PedidosModel->getDetalle($_POST['idPedido']));
+                $this->display("mostrarDetalle.twig");
+            }
+            catch (Exception $e)
+            {
+                $this->addData('mensajeError', $e->getMessage());
+                $this->display('GestionPedidosListarTemplate.twig');
+            }
         }
-        catch (Exception $e)
-        {
-            $this->addData('mensajeError', $e -> getMessage());
-            $this->display('GestionPedidosListarTemplate.twig');
-        }
-
-
     }
 
-    public function formCancelarPedido(){
-        $this->token();
-        $this->addData('id', $_POST['idPedido']);
-        $this->display('cancerlarPedido.twig');
+    public function formCancelarPedido()
+    {
+        if($this->permissions())
+        {
+            $this->token();
+            $this->addData('id', $_POST['idPedido']);
+            $this->display('cancerlarPedido.twig');
+        }
     }
 
     public function cancelarPedido()
     {
-        try
+        if($this->permissions())
         {
-            if (! isset($_POST['tokenScrf'])) throw new valException("no hay un token de validación");
-            if (! $this->tokenIsValid($_POST['tokenScrf'])) throw new valException("el token no es valido");
-            $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?",3);
-            $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
-            if(!$pedido) throw new valException('El pedido no es valido');
-            if(($pedido->idEstado != "pendiente"))throw new valException("El pedido no cumple los requisitos para ser cancelado");
-            $detalles = $this->PedidosModel->getDetalle($_POST['idPedido']);
-            $this->PedidosModel->actualizarEstado("cancelado", $_POST['idPedido']);
-
-            if(strlen($_POST['comentario']) > 0 ) $this->PedidosModel->actualizarComentario($_POST['comentario'], $_POST['idPedido']);
-
-            foreach ($detalles as $detalle)
+            try
             {
+                if (!isset($_POST['tokenScrf'])) throw new valException("no hay un token de validación");
+                if (!$this->tokenIsValid($_POST['tokenScrf'])) throw new valException("el token no es valido");
+                $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?", 3);
+                $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
+                if (!$pedido) throw new valException('El pedido no es valido');
+                if (($pedido->idEstado != "pendiente")) throw new valException("El pedido no cumple los requisitos para ser cancelado");
+                $detalles = $this->PedidosModel->getDetalle($_POST['idPedido']);
+                $this->PedidosModel->actualizarEstado("cancelado", $_POST['idPedido']);
 
-                $this->PedidosModel->actualizarCantProductos($detalle->idProducto, $detalle->stock + $detalle->cantidad);
+                if (strlen($_POST['comentario']) > 0) $this->PedidosModel->actualizarComentario($_POST['comentario'], $_POST['idPedido']);
+
+                foreach ($detalles as $detalle)
+                {
+                    $this->PedidosModel->actualizarCantProductos($detalle->idProducto, $detalle->stock + $detalle->cantidad);
+                }
+
+                $_GET['pag'] = 0;
+
+                $this->verPedidos();
+
             }
-
-            $_GET['pag'] = 0;
-
-            $this->verPedidos();
-
+            catch (Exception $e)
+            {
+                $this->addData('mensajeError', $e->getMessage());
+                $this->addData('id', $_POST['idPedido']);
+                $this->display('cancerlarPedido.twig');
+            }
         }
-        catch (Exception $e)
-        {
-            $this->addData('mensajeError', $e -> getMessage());
-            $this->addData('id', $_POST['idPedido']);
-            $this->display('cancerlarPedido.twig');
-        }
-
-
     }
 
-    public function formAceptarPedido(){
-        $this->token();
-        $this->addData('id', $_POST['idPedido']);
-        $this->display('aceptarPedido.twig');
-    }
-
-    public function aceptarPedido(){
-        try
+    public function formAceptarPedido()
+    {
+        if($this->permissions())
         {
-            if (! isset($_POST['tokenScrf'])) throw new valException("no hay un token de validación");
-            if (! $this->tokenIsValid($_POST['tokenScrf'])) throw new valException("el token no es valido");
-            $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?",3);
-            $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
-            if(!$pedido) throw new valException('El pedido no es valido');
-            if(($pedido->idEstado != "pendiente"))throw new valException("El pedido no cumple los requisitos para ser cancelado");
-            $detalles = $this->PedidosModel->getDetalle($_POST['idPedido']);
-            $this->PedidosModel->actualizarEstado("Entregado", $_POST['idPedido']);
-
-            if(strlen($_POST['comentario']) > 0 ) $this->PedidosModel->actualizarComentario($_POST['comentario'], $_POST['idPedido']);
-
-            foreach ($detalles as $detalle)
-            {
-
-                $venta['idProducto'] = $detalle->idProducto;
-                $venta['precioVentaUnitario'] = $detalle->precioVentaUnitario;
-                $venta['cant'] = $detalle->cantidad;
-
-                $this->VentaModel->insertarVentaId($venta);
-            }
-
-            $_GET['pag'] = 0;
-
-            $this->verPedidos();
-        }
-        catch (Exception $e)
-        {
-            $this->addData('mensajeError', $e -> getMessage());
+            $this->token();
             $this->addData('id', $_POST['idPedido']);
             $this->display('aceptarPedido.twig');
         }
+    }
 
+    public function aceptarPedido()
+    {
+        if ($this->permissions())
+        {
+            try
+            {
+                if (!isset($_POST['tokenScrf'])) throw new valException("no hay un token de validación");
+                if (!$this->tokenIsValid($_POST['tokenScrf'])) throw new valException("el token no es valido");
+                $this->validator->validarNumeros($_POST['idPedido'], "Que estás tocando picaron?", 3);
+                $pedido = $this->PedidosModel->getPedido($_POST['idPedido']);
+                if (!$pedido) throw new valException('El pedido no es valido');
+                if (($pedido->idEstado != "pendiente")) throw new valException("El pedido no cumple los requisitos para ser cancelado");
+                $detalles = $this->PedidosModel->getDetalle($_POST['idPedido']);
+                $this->PedidosModel->actualizarEstado("Entregado", $_POST['idPedido']);
 
+                if (strlen($_POST['comentario']) > 0) $this->PedidosModel->actualizarComentario($_POST['comentario'], $_POST['idPedido']);
 
+                foreach ($detalles as $detalle)
+                {
+                    $venta['idProducto'] = $detalle->idProducto;
+                    $venta['precioVentaUnitario'] = $detalle->precioVentaUnitario;
+                    $venta['cant'] = $detalle->cantidad;
+                    $this->VentaModel->insertarVentaId($venta);
+                }
+                $_GET['pag'] = 0;
+                $this->verPedidos();
+            }
+            catch (Exception $e)
+            {
+                $this->addData('mensajeError', $e->getMessage());
+                $this->addData('id', $_POST['idPedido']);
+                $this->display('aceptarPedido.twig');
+            }
+        }
     }
 }
