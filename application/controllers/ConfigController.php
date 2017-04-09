164177058b3bbcf9ff7bc34fb51df87ecbba8cc9
parinisantiago <parinisantiago@gmail.com>
2017-04-08 23:07:18 -0300

    control de permisos para ConfigController

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -4,17 +4,18 @@ include_once('MainController.php');
 class ConfigController extends Controller
 {
 
-
-
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function index(){
-        $this->display("IndexTemplate.twig");
+    public function index()
+    {
+        if($this->permissions()) $this->display("IndexTemplate.twig");
     }
 
-    public function setMensajeError($error){
+    public function setMensajeError($error)
+    {
         $this->addData('mensajeError', $error);
         $this->index();
     }
@@ -27,32 +28,41 @@ class ConfigController extends Controller
 
     public function configuracionSitio()
     {
-        $this->display("ConfiguracionTemplate.twig");
+        if($this->permissions()) $this->display("ConfiguracionTemplate.twig");
     }
 
     public function changeConf()
     {
-        $this->validarConf();
-        $this->conf->updateConf($_POST);
-        $this->addData('config', $this->conf->getConfiguracion());
-        $this->display("ConfiguracionTemplate.twig");
+        if($this->permissions())
+        {
+            $this->validarConf();
+            $this->conf->updateConf($_POST);
+            $this->addData('config', $this->conf->getConfiguracion());
+            $this->display("ConfiguracionTemplate.twig");
+        }
     }
 
     private function validarConf()
     {
-        try {
-            if (!isset($_POST['submitButton'])) throw new Exception('Apreta el boton de modificar macho');
-            if (!isset($_POST['titulo'])) throw new Exception('Falta escribir el titulo');
-            elseif (!preg_match("/^[a-zA-Z0-9 ]+$/", $_POST['titulo'])) throw new Exception('Valor del titulo no valido');
-            if (!isset($_POST['descripcion'])) throw new Exception('Falta escribir el descripcion');
-            elseif (!preg_match("/^[a-zA-Z0-9 ;.,àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹ]+$/", $_POST['descripcion'])) throw new Exception('Valor de la  descripcion no valido');
-            if (!isset($_POST['mensaje'])) throw new Exception('Falta escribir el mensaje');
-            elseif (!preg_match("/^[a-zA-Z ]+$/", $_POST['mensaje'])) throw new Exception('Valor del mensaje no valido');
-            if (!isset($_POST['lista'])) throw new Exception('Falta escribir el numero de la lista');
-            elseif (!preg_match("/^[0-9]+$/", $_POST['lista'])) throw new Exception('Valor del numero de la lista no valido');
-        } catch (Exception $e){
-            $controller= new MainController();
-            $controller->index();
+        if($this->permissions())
+        {
+            try
+            {
+                if (!isset($_POST['submitButton'])) throw new Exception('Apreta el boton de modificar macho');
+                if (!isset($_POST['titulo'])) throw new Exception('Falta escribir el titulo');
+                elseif (!preg_match("/^[a-zA-Z0-9 ]+$/", $_POST['titulo'])) throw new Exception('Valor del titulo no valido');
+                if (!isset($_POST['descripcion'])) throw new Exception('Falta escribir el descripcion');
+                elseif (!preg_match("/^[a-zA-Z0-9 ;.,àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹ]+$/", $_POST['descripcion'])) throw new Exception('Valor de la  descripcion no valido');
+                if (!isset($_POST['mensaje'])) throw new Exception('Falta escribir el mensaje');
+                elseif (!preg_match("/^[a-zA-Z ]+$/", $_POST['mensaje'])) throw new Exception('Valor del mensaje no valido');
+                if (!isset($_POST['lista'])) throw new Exception('Falta escribir el numero de la lista');
+                elseif (!preg_match("/^[0-9]+$/", $_POST['lista'])) throw new Exception('Valor del numero de la lista no valido');
+            }
+            catch (Exception $e)
+            {
+                $controller = new MainController();
+                $controller->index();
+            }
         }
     }
 }
