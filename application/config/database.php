43bfb3398e36d456b9c2afe9702300d487e0ae57
parinisantiago <parinisantiago@gmail.com>
2017-05-09 19:51:01 -0300

    config database

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost; dbname=proyecto; charset=utf8',
+	'dsn'	=> 'mysql:host=localhost; dbname=grupo64; charset=utf8',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
+	'username' => 'grupo64',
+	'password' => 'leeGhish8e',
 	'database' => '',
 	'dbdriver' => 'pdo',
 	'dbprefix' => '',
